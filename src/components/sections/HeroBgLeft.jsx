d302d6e4fe1259d639be460a4bcb3a0858b48aa0
Edison Matos <edison.matos@live.com>
2024-07-05 15:26:03 -0300

    feat: Atualiza svg container da hero

--- src/components/sections/HeroBgLeft.jsx
+++ src/components/sections/HeroBgLeft.jsx
@@ -61,7 +61,7 @@ export default function HeroBgLeft() {
         </div>
       </div>
       <div className="bg-black bg-opacity-70">
-        <div class="custom-shape-divider-bottom-1720203688">
+        <div class="custom-shape-divider-bottom-1720203688 hidden desktop1:block">
           <svg
             data-name="Layer 1"
             xmlns="http://www.w3.org/2000/svg"
@@ -70,6 +70,19 @@ export default function HeroBgLeft() {
           >
             <path
               d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+              class="shape-fill"
+            ></path>
+          </svg>
+        </div>
+        <div class="custom-shape-divider-bottom-1720203865 desktop1:hidden">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M598.97 114.72L0 0 0 120 1200 120 1200 0 598.97 114.72z"
               class="shape-fill"
             ></path>
           </svg>
