464a100c78c9285afc5a56bd6d406a9e26f92f97
Gabriel <gabrielsouza65115@gmail.com>
2024-07-17 11:56:45 -0300

    feat: Insere a lógica do SectionHeader

--- src/components/sectionElements/SectionHeader.jsx
+++ src/components/sectionElements/SectionHeader.jsx
@@ -10,29 +10,34 @@ export default function SectionHeader({
   miniTitleTextColor,
 }) {
   if (color === "dark") {
-    miniTitleBgColor === "bg-[#682121]";
-    miniTitleTextColor === "bg-primary";
+    miniTitleBgColor = "bg-primary opacity-10";
+    miniTitleTextColor = "text-secondary";
+    titleColor = "text-secondary";
+    subtitleColor = "text-darker opacity-50";
   } else {
-    miniTitleBgColor === "bg-[#FFFFFF]";
-    miniTitleTextColor === "bg-secondary";
+    miniTitleBgColor = "bg-secondary";
+    miniTitleTextColor = "text-lighter";
+    titleColor = "text-lighter";
+    subtitleColor = "text-lighter";
   }
+
 
   return (
     <div
       className={`w-[90%] tablet1:w-[80%] desktop1:w-[60%] max-w-[920px] ${className}`}
     >
       <div
-        className={`py-[4px] font-semibold px-[8px] text-paragraph2 rounded-2xl inline-block bg-opacity-10 mb-[16px] ${miniTitleBgColor}`}
+        className={`py-[4px] font-semibold px-[8px] text-paragraph2 rounded-2xl inline-block mb-[16px] ${miniTitleBgColor}`}
       >
         <p className={`${miniTitleTextColor}`}>{miniTitle}</p>
       </div>
       <h1
-        className={`${titleColor} text-black text-title4 leading-[34px] tablet1:leading-[42px] tablet1:text-title5 font-mainFont font-bold mb-[16px]`}
+        className={`${titleColor}  text-title4 leading-[34px] tablet1:leading-[42px] tablet1:text-title5 font-mainFont font-bold mb-[16px]`}
       >
         {sectionHeaderTitle}
       </h1>
       <p
-        className={`text-quaternary opacity-70 mb-[26px] tablet1:mb-[40px] desktop1:mb-[72px] text-title1 font-secondFont ${subtitleColor}`}
+        className={` mb-[26px] tablet1:mb-[40px] desktop1:mb-[72px] text-title1 font-secondFont ${subtitleColor}`}
       >
         {sectionHeaderSubtitle}
       </p>
