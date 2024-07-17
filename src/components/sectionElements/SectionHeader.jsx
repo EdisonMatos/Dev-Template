adbf2cbeaf5cbffe40a900bfdfa01a653fa07a45
Edison Matos <edison.matos@live.com>
2024-07-17 09:00:43 -0300

    feat: Implementa propriedades pra sectionHeader

--- src/components/sectionElements/SectionHeader.jsx
+++ src/components/sectionElements/SectionHeader.jsx
@@ -1,8 +1,3 @@
-import MotionDivDownToUp from "../animation/MotionDivDownToUp";
-import PropTypes from "prop-types";
-import SectionTitles from "./SectionTitles";
-import SectionSubtitles from "./SectionSubtitles";
-
 export default function SectionHeader(props) {
   const sectionHeaderTitle = props.sectionHeaderTitle;
   const sectionHeaderSubtitle = props.sectionHeaderSubtitle;
@@ -10,19 +5,24 @@ export default function SectionHeader(props) {
   const titleColor = props.titleColor;
   const subtitleColor = props.subtitleColor;
   const miniTitle = props.miniTitle;
+  const color = props.color;
+  const miniTitleBgColor = props.miniTitleBgColor;
+  const miniTitleTextColor = props.miniTitleTextColor;
 
-  SectionHeader.propTypes = {
-    sectionHeaderTitle: PropTypes.any,
-    sectionHeaderSubtitle: PropTypes.any,
-    className: PropTypes.any,
-  };
+  if (color === "dark") {
+    miniTitleBgColor === "bg-[#682121]";
+    miniTitleTextColor === "bg-primary";
+  } else {
+  }
 
   return (
     <div
       className={`w-[90%] tablet1:w-[80%] desktop1:w-[60%] max-w-[920px] ${className}`}
     >
-      <div className="py-[4px] font-semibold px-[8px] text-paragraph2 rounded-2xl inline-block bg-[#682121] bg-opacity-10 mb-[16px]">
-        <p className="text-primary">{miniTitle}</p>
+      <div
+        className={`py-[4px] font-semibold px-[8px] text-paragraph2 rounded-2xl inline-block bg-opacity-10 mb-[16px] ${miniTitleBgColor}`}
+      >
+        <p className={`${miniTitleTextColor}`}>{miniTitle}</p>
       </div>
       <h1
         className={`${titleColor} text-black text-title4 leading-[34px] tablet1:leading-[42px] tablet1:text-title5 font-mainFont font-bold mb-[16px]`}
