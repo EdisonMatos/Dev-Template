9aa6baaf849138140f53193e45750411f21638ef
Gabriel <gabrielsouza65115@gmail.com>
2024-07-19 10:18:49 -0300

    feat: Cria a lógica de padding da SectionArea

--- src/components/sectionElements/SectionArea.jsx
+++ src/components/sectionElements/SectionArea.jsx
@@ -1,17 +1,30 @@
 import PropTypes from "prop-types";
 import { Children } from "react";
 
-export default function SectionArea({ children, className, id }) {
+export default function SectionArea({ children, className, id, topSectionArea = true, botSectionArea = true, paddingTopAndBottom }) {
   SectionArea.propTypes = {
     children: PropTypes.any,
     className: PropTypes.string,
-    id: PropTypes.any,
+    id: PropTypes.string,
+    topSectionArea: PropTypes.bool,
+    botSectionArea: PropTypes.bool,
+    paddingTopAndBottom: PropTypes.bool,
   };
-  const childrenArray = Children.toArray(children);
+
+  if (paddingTopAndBottom === false) {
+    topSectionArea = paddingTopAndBottom;
+    botSectionArea = paddingTopAndBottom;
+  }
+
+
+  const childrenArray = Children.toArray(children); 
+  const paddingTop = topSectionArea ? 'pt-[40px] tablet1:pt-[64px] desktop1:pt-[96px]' : ''; 
+  const paddingBottom = botSectionArea ? 'pb-[40px] tablet1:pb-[64px] desktop1:pb-[96px]' : ''; 
+
   return (
     <div
       id={id}
-      className={`w-full flex flex-col items-center py-[40px] tablet1:py-[64px] desktop1:py-[96px] ${className}`}
+      className={`w-full flex flex-col items-center ${paddingTop} ${paddingBottom} ${paddingTopAndBottom} ${className}`}
     >
       {childrenArray}
     </div>
