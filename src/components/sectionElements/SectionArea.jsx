fb56472fde79a3ba8120ff957071e62d403af197
Gabriel <gabrielsouza65115@gmail.com>
2024-07-22 09:45:30 -0300

    fix: Ajusta os nomes das propriedades da SectionArea

--- src/components/sectionElements/SectionArea.jsx
+++ src/components/sectionElements/SectionArea.jsx
@@ -1,30 +1,30 @@
 import PropTypes from "prop-types";
 import { Children } from "react";
 
-export default function SectionArea({ children, className, id, topSectionArea = true, botSectionArea = true, paddingTopAndBottom }) {
+export default function SectionArea({ children, className, id, paddingtop = true, paddingbot = true, paddingTopAndBottom }) {
   SectionArea.propTypes = {
     children: PropTypes.any,
     className: PropTypes.string,
     id: PropTypes.string,
-    topSectionArea: PropTypes.bool,
-    botSectionArea: PropTypes.bool,
+    paddingtop: PropTypes.bool,
+    paddingbot: PropTypes.bool,
     paddingTopAndBottom: PropTypes.bool,
   };
 
   if (paddingTopAndBottom === false) {
-    topSectionArea = paddingTopAndBottom;
-    botSectionArea = paddingTopAndBottom;
+    paddingtop = paddingTopAndBottom;
+    paddingbot = paddingTopAndBottom;
   }
 
 
   const childrenArray = Children.toArray(children); 
-  const paddingTop = topSectionArea ? 'pt-[40px] tablet1:pt-[64px] desktop1:pt-[96px]' : ''; 
-  const paddingBottom = botSectionArea ? 'pb-[40px] tablet1:pb-[64px] desktop1:pb-[96px]' : ''; 
+  const paddingTopp = paddingtop ? 'pt-[40px] tablet1:pt-[64px] desktop1:pt-[96px]' : ''; 
+  const paddingBottom = paddingbot ? 'pb-[40px] tablet1:pb-[64px] desktop1:pb-[96px]' : ''; 
 
   return (
     <div
       id={id}
-      className={`w-full flex flex-col items-center ${paddingTop} ${paddingBottom} ${paddingTopAndBottom} ${className}`}
+      className={`w-full flex flex-col items-center ${paddingTopp} ${paddingBottom} ${paddingTopAndBottom} ${className}`}
     >
       {childrenArray}
     </div>
