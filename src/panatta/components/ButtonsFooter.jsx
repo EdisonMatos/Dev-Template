21171e19f6f1264eefcd7b70e878f744bb6832a7
Edison Matos <edison.matos@live.com>
2024-07-24 15:01:24 -0300

    feat: Insere tigre e alinha

---
+++ src/panatta/components/ButtonsFooter.jsx
@@ -0,0 +1,11 @@
+export default function ButtonsFooter({ label, buttonLink }) {
+  return (
+    <a ref={buttonLink} target="_blank">
+      <button className="bg-gradient-to-b from-[#2FFF76] to-[#1C9947] rounded-[23px] py-[7px] px-[7px]">
+        <div className="rounded-[20px] border-solid border-[1px] border-[#112313] py-[10px] px-[27.5px] text-[17px] italic font-extrabold text-[#112313]">
+          {label}
+        </div>
+      </button>
+    </a>
+  );
+}
--- src/panatta/sections/FooterPanatta.jsx
+++ src/panatta/sections/FooterPanatta.jsx
@@ -1,3 +1,36 @@
+import jogueEGanhe from "../img/footer/jogueEGanhe.png";
+import logoPanatta from "../img/footer/logoFooter.png";
+import ButtonsFooter from "../components/ButtonsFooter";
+
 export default function FooterPanatta() {
-  return <div className="text-white">Footer</div>;
+  return (
+    <div className="w-[90%] max-w-[1198px] flex flex-col items-center">
+      <div className="flex">
+        <div className="w-[352px] flex flex-col justify-end ">
+          <img
+            src={logoPanatta}
+            alt="Logo Panatta Casino"
+            className="mb-[57px]"
+          ></img>
+          <h1 className="text-[45.5px] font-bold mb-[23px]">REDES SOCIAIS</h1>
+          <div className="flex gap-[21.2px] mb-[61px]">
+            <ButtonsFooter label="Instagram" />
+            <ButtonsFooter label="Whatsapp" />
+          </div>
+        </div>
+        <div className=" relative">
+          <img
+            src={jogueEGanhe}
+            alt="Imagem ilustrativa de jogue e ganhe"
+            className="relative left-[102px] w-[1008px] h-[auto]"
+          ></img>
+        </div>
+      </div>
+      <div className=" italic text-[15px] h-[86px] flex items-center w-full justify-center">
+        Copyright © 2024 - Todos os direitos reservados.
+      </div>
+    </div>
+  );
 }
+
+// Paddings dos botões escolhidos pra serem simétricos
