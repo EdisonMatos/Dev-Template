5acf6963de84c0aff7a02d6949bccbd693925ac6
Edison Matos <edison.matos@live.com>
2024-07-24 18:52:55 -0300

    feat: Insere animação de scale na hero

--- src/panatta/sections/HeroPanatta.jsx
+++ src/panatta/sections/HeroPanatta.jsx
@@ -1,31 +1,42 @@
-import Logo from '../../assets/imgs/logo/Logotipo1.png';
-import ImgHero from '../../assets/imgs/hero/Hero.png';
-import Ball from '../../assets/imgs/hero/Ball.png';
-import Effect from '../../assets/imgs/hero/Effect.png';
+import Logo from "../../assets/imgs/logo/Logotipo1.png";
+import ImgHero from "../../assets/imgs/hero/Hero.png";
+import Ball from "../../assets/imgs/hero/Ball.png";
+import Effect from "../../assets/imgs/hero/Effect.png";
 
 export default function HeroPanatta() {
   return (
     <div className="w-full relative">
-    <img className='absolute inset-0 m-auto -mt-0' src={Effect} alt="Efeito Matrix" />
-    <img
-      className="absolute top-0 left-1/2 transform -translate-x-1/2"
-      src={Ball}
-      alt="Bola girando"
-    />
-    <div className="w-full h-[138px] absolute top-0 bg-[#2FFF76]">
-      <div className="h-[117px] flex items-center justify-center relative top-[18px] bg-[#112313]">
-        <img className="relative z-10" src={Logo} alt="Logotipo Panatta Cassino" />
+      <img
+        className="absolute inset-0 m-auto -mt-0"
+        src={Effect}
+        alt="Efeito Matrix"
+      />
+      <img
+        className="absolute top-0 left-1/2 transform -translate-x-1/2"
+        src={Ball}
+        alt="Bola girando"
+      />
+      <div className="w-full h-[138px] absolute top-0 bg-[#2FFF76]">
+        <div className="h-[117px] flex items-center justify-center relative top-[18px] bg-[#112313]">
+          <img
+            className="relative z-10"
+            src={Logo}
+            alt="Logotipo Panatta Cassino"
+          />
+        </div>
+      </div>
+      <div className="mb-[112px] flex flex-col justify-center items-center">
+        <img
+          className="mb-4 mt-[399px] w-[1085.58px] h-[415.65px]"
+          src={ImgHero}
+          alt="Hero Panatta Cassino"
+        />
+        <div className="border-2 border-[#ABFFC8] hover:scale-110 transition p-1">
+          <button className="px-4 py-2 font-bold text-[#112313] bg-gradient-to-r from-[#2FFF76] to-[#0F652C] w-[558px] h-[120px] text-[63px] font-[Antonio] border-2 border-[#ABFFC8]">
+            RESGATE SEU BÔNUS
+          </button>
+        </div>
       </div>
     </div>
-    <div className="mb-[112px] flex flex-col justify-center items-center">
-    <img className="mb-4 mt-[399px] w-[1085.58px] h-[415.65px]" src={ImgHero} alt="Hero Panatta Cassino" />
-     <div className='border-2 border-[#ABFFC8] p-1'> 
-      <button
-        className="px-4 py-2 font-bold text-[#112313] bg-gradient-to-r from-[#2FFF76] to-[#0F652C] w-[558px] h-[120px] text-[63px] font-[Antonio] border-2 border-[#ABFFC8]">
-          RESGATE SEU BÔNUS
-          </button>
-          </div>
-    </div>
-  </div>
   );
 }
