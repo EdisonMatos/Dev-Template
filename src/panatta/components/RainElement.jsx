842c7bd00ad83a8b36606225ebe61725d1539303
Gabriel <gabrielsouza65115@gmail.com>
2024-07-26 10:47:29 -0300

    fix: Ajusta o tempo de execução do efeito de chuva

--- src/panatta/components/RainElement.jsx
+++ src/panatta/components/RainElement.jsx
@@ -17,11 +17,11 @@ const RainElement = () => {
       const randoHundo = Math.floor(Math.random() * (98 - 1 + 1) + 1);
       const randoFiver = Math.floor(Math.random() * (5 - 2 + 1) + 2);
       increment += randoFiver;
-
+      // velocidade do efeito
       drops += `<div class="drop" style="left: ${increment}%; bottom: ${
         randoFiver + randoFiver - 1 + 100
-      }%; animation-delay: 0.${randoHundo}s; animation-duration: 0.5${randoHundo}s;">
-                  <div class="stem" style="animation-delay: 0.${randoHundo}s; animation-duration: 0.5${randoHundo}s;"></div>
+      }%; animation-delay: 2.${randoHundo}s; animation-duration: 0.9${randoHundo}s;">
+                  <div class="stem" style="animation-delay: 2.${randoHundo}s; animation-duration:0.9${randoHundo}s;"></div>
                 </div>`;
     }
 
