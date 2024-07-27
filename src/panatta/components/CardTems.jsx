0d369b7985c4b653484ec826e80261ee167d8622
Edison Matos <edison.matos@live.com>
2024-07-27 11:25:06 -0300

    feat: Faz responsividade dos termos

--- src/panatta/components/CardTems.jsx
+++ src/panatta/components/CardTems.jsx
@@ -1,8 +1,10 @@
 export default function CardTerms({ title, paragraph }) {
   return (
-    <div className="containerTítuloETexto w-full pl-[69px] pr-[97px] mb-[26px]">
-      <h1 className="font-bold text-[30px] mb-[4px] leading-[30px]">{title}</h1>
-      <p className="text-[27px]">{paragraph}</p>
+    <div className=" w-full pl-[30px] tablet1:pl-[69px] pr-[30px] tablet1:pr-[97px] mb-[26px]">
+      <h1 className="font-bold text-[20px] tablet1:text-[30px] mb-[4px] leading-[30px]">
+        {title}
+      </h1>
+      <p className="text-[16px] tablet1:text-[27px]">{paragraph}</p>
     </div>
   );
 }
--- src/panatta/sections/TermsPanatta.jsx
+++ src/panatta/sections/TermsPanatta.jsx
@@ -3,16 +3,16 @@ import CardTerms from "../components/CardTems";
 export default function TermsPanatta() {
   return (
     <div className="w-[90%] max-w-[1198px] flex flex-col items-center">
-      <div className="container pt-[157px] pb-[180px] flex flex-col items-center">
-        <div className="flex items-center header mb-[16px] w-full">
-          <div className="flex-grow border-t border-white border-solid border-[3px]"></div>
-          <h1 className="mx-[48px] text-[45px] font-[Anaheim] text-center">
+      <div className="pt-[157px] pb-[180px] flex flex-col items-center">
+        <div className="flex items-center justify-center header mb-[16px] w-full">
+          <div className="hidden phone3:flex flex-grow border-t border-white border-solid border-[3px]"></div>
+          <h1 className="mx-[20px] tablet1:mx-[48px] text-[28px] tablet1:text-[45px] font-[Anaheim] text-center">
             Termos e condições
           </h1>
-          <div className="flex-grow border-t border-white border-solid border-[3px]"></div>
+          <div className="hidden phone3:flex flex-grow border-t border-white border-solid border-[3px]"></div>
         </div>
 
-        <div className="ContainerBloco border-[1px] border-solid bg-[#13171B] border-[#2FFF76] rounded-[14px] py-[64px] font-[Inter]">
+        <div className="border-[1px] border-solid bg-[#13171B] border-[#2FFF76] rounded-[14px] pt-[32px] pb-[6px] tablet1:pt-[64px] tablet1:pb-[32px] font-[Inter]">
           <CardTerms
             title="Regras e disposições"
             paragraph="O bônus será creditado em sua conta caso você perca o valor integral do primeiro depósito. Para realizar o saque, é necessário cumprir o requisito de rollover de uma vez o valor do bônus e realizar, no mínimo, 100
