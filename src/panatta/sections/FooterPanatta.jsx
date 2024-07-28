cab968716360c876f575f46d5cce7e9ad63eb44d
Edison Matos <edison.matos@live.com>
2024-07-28 11:21:36 -0300

    feat: Corrige responsividade do footer

--- src/panatta/sections/FooterPanatta.jsx
+++ src/panatta/sections/FooterPanatta.jsx
@@ -11,22 +11,22 @@ export default function FooterPanatta() {
         className="relative right-[20px] desktop1:absolute desktop1:right-[-120px] desktop1:top-[0px] "
       ></img>
       <div className="flex justify-center desktop1:justify-start">
-        <div className="w-[352px] flex flex-col justify-end ">
+        <div className="w-[90%] desktop1:w-[352px] flex flex-col justify-end ">
           <img
             src={logoPanatta}
             alt="Logo Panatta Casino"
-            className="mb-[57px] w-[325px]"
+            className="mb-[57px] w-full desktop1:w-[325px]"
           ></img>
-          <h1 className="text-[45.5px] mb-[23px] w-[355px] font-black leading-[40px]">
+          <h1 className="text-[28px] text-center desktop1:text-[45.5px] mb-[23px] desktop1:w-[355px] font-black leading-[40px]">
             REDES SOCIAIS
           </h1>
-          <div className="flex justify-between mb-[61px]">
+          <div className="flex gap-[20px] phone3:gap-[0px] items-center flex-col phone3:flex-row justify-between mb-[32px] desktop1:mb-[61px]">
             <ButtonsFooter label="Instagram" />
             <ButtonsFooter label="Whatsapp" />
           </div>
         </div>
       </div>
-      <div className=" italic text-[15px] h-[86px] flex items-center w-full justify-center opacity-80">
+      <div className=" italic text-[15px] h-[86px] flex items-center text-center w-full justify-center opacity-80">
         Copyright © 2024 - Todos os direitos reservados.
       </div>
     </div>
