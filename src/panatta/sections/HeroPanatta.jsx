df0f10ee2360fef2a11a5fc8f5a72625f1b2c734
Gabriel <gabrielsouza65115@gmail.com>
2024-07-29 09:42:18 -0300

    feat: Insere um efeito de iluminação sobreposto a bola

--- src/panatta/sections/HeroPanatta.jsx
+++ src/panatta/sections/HeroPanatta.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import Logo from "../../assets/imgs/logo/Logotipo1.png";
 import ImgHero from "../../assets/imgs/hero/Hero.png";
-import '../../indexPanatta.css';
+import "../../indexPanatta.css";
 import RainElement from "../components/RainElement";
 import Ball from "../../assets/imgs/hero/Bola.png";
 import EffectBg from "../../assets/imgs/hero/EfeitoLuz.png";
-
+import bgEffect from "../img/bg/bgEffect.png";
 
 export default function HeroPanatta() {
   return (
@@ -17,7 +17,13 @@ export default function HeroPanatta() {
         alt="Efeito Iluminação de fundo"
       />
 
-      <img className="absolute right-[35%] bottom-[65%] h-auto w-[30%] spin" src={Ball} alt="Bola girando" />
+      <img
+        className="absolute right-[35%] bottom-[65%] h-auto w-[30%] spin"
+        src={Ball}
+        alt="Bola girando"
+      />
+
+      <img src={bgEffect} className="absolute bottom-[55%] h-auto w-full" alt="efeito sobreposto" />
 
       <div className="w-full h-[138px] absolute top-0 bg-[#2FFF76]">
         <div className="h-[117px] flex items-center justify-center relative top-[18px] bg-[#112313]">
@@ -43,5 +49,3 @@ export default function HeroPanatta() {
     </div>
   );
 }
-
-
