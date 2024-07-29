a66a7b45818cf10c246ebaac92d5ccfc19d5af27
Gabriel <gabrielsouza65115@gmail.com>
2024-07-29 09:53:33 -0300

    fix: Coloca o efeito de iluminação atrás da Bola, porém, sobre todo o restante

--- src/panatta/sections/HeroPanatta.jsx
+++ src/panatta/sections/HeroPanatta.jsx
@@ -16,14 +16,18 @@ export default function HeroPanatta() {
         src={EffectBg}
         alt="Efeito Iluminação de fundo"
       />
+      <img
+        src={bgEffect}
+        className="absolute bottom-[55%] h-auto w-full"
+        alt="efeito sobreposto"
+      />
 
       <img
         className="absolute right-[35%] bottom-[65%] h-auto w-[30%] spin"
         src={Ball}
         alt="Bola girando"
       />
-
-      <img src={bgEffect} className="absolute bottom-[55%] h-auto w-full" alt="efeito sobreposto" />
+      
 
       <div className="w-full h-[138px] absolute top-0 bg-[#2FFF76]">
         <div className="h-[117px] flex items-center justify-center relative top-[18px] bg-[#112313]">
