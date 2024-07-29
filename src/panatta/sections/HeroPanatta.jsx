81de3b8720e1efc26e5d246d158493a859c31439
Edison Matos <edison.matos@live.com>
2024-07-29 14:25:46 -0300

    feat: Ajustes na Hero

--- src/panatta/sections/HeroPanatta.jsx
+++ src/panatta/sections/HeroPanatta.jsx
@@ -12,7 +12,7 @@ export default function HeroPanatta() {
     <div className="relative w-full">
       <RainElement />
       <img
-        className=" absolute phone1:w-full phone1:mt-[40%] phone2:mt-[35%] tablet1:mt-[20%] tablet2:mt-[15%] desktop1:inset-0 desktop1:m-auto desktop1:-mt-0"
+        className=" absolute phone1:w-full phone1:mt-[40%] phone2:mt-[120px] tablet1:mt-[120px] tablet2:mt-[100px] desktop1:inset-0 desktop1:m-auto desktop1:-mt-0"
         src={EffectBg}
         alt="Efeito Iluminação de fundo"
       />
@@ -23,7 +23,7 @@ export default function HeroPanatta() {
       />
 
       <img
-        className="absolute phone1:bottom-[70%] phone1:right-[20%] phone1:w-[60%] phone2:bottom-[69%] tablet1:w-[40%] tablet1:bottom-[65%] tablet1:right-[30%] desktop1:right-[35%] desktop1:bottom-[78%] desktop1:h-auto desktop1:w-[30%] desktop2:right-[35%] desktop2:bottom-[73%] desktop2:h-auto desktop2:w-[30%] spin"
+        className="absolute phone1:bottom-[70%] phone1:right-[20%] phone1:w-[60%] phone2:bottom-[67%] tablet1:w-[40%] tablet1:bottom-[65%] tablet1:right-[30%] desktop1:right-[35%] desktop1:bottom-[78%] desktop1:h-auto desktop1:w-[30%] desktop2:right-[35%] desktop2:bottom-[73%] desktop2:h-auto desktop2:w-[30%] spin"
         src={Ball}
         alt="Bola girando"
       />
@@ -39,11 +39,11 @@ export default function HeroPanatta() {
       </div>
       <div className="mb-[112px] flex flex-col justify-center items-center">
         <img
-          className="relative phone1:mt-[350px] phone1:w-[300px] phone1:h-[200px] phone1:px-[2px] desktop1:mb-4 desktop1:px-[20px] desktop1:mt-[400px] desktop1:w-[1085.58px] desktop1:h-[415.65px]"
+          className="relative phone1:mt-[300px] w-[70%] phone1:px-[2px] desktop1:mb-4 desktop1:px-[20px] desktop1:mt-[400px] desktop1:w-[1085.58px]"
           src={ImgHero}
           alt="Hero Panatta Cassino"
         />
-        <div className="phone1:mt-[5%] border-2 border-[#ABFFC8] hover:scale-110 transition p-1">
+        <div className="mt-[60px] desktop1:mt-[5%] border-2 border-[#ABFFC8] hover:scale-110 transition p-1">
           <button className="relative z-20 px-4 py-2 font-bold text-[#112313] bg-gradient-to-r from-[#2FFF76] to-[#0F652C] phone1:w-full phone1:text-[35px] desktop1:w-full desktop1:h-[120px] desktop1:text-[63px] font-[Antonio] border-2 border-[#ABFFC8]">
             RESGATE SEU BÔNUS
           </button>
