c5c91203976f95838bfd09981c5f560a93ca205e
Edison Matos <edison.matos@live.com>
2024-08-04 10:32:23 -0300

    feat: Insere onda abaixo de HowItworks

--- src/components/sections/HowItWorksImage.jsx
+++ src/components/sections/HowItWorksImage.jsx
@@ -23,7 +23,7 @@ export default function HowItWorksImage() {
   const [modalTitle, setModalTitle] = useState("");
 
   return (
-    <SectionArea id="about" className="" paddingtop={false}>
+    <SectionArea id="about" className="" paddingTopAndBottom={false}>
       <SectionWrapper className="flex flex-col desktop1:flex-row-reverse gap-[40px] desktop1:gap-x-[60px] desktop2:gap-0 desktop1:justify-between">
         <MotionDivDownToUp className="w-[100%] desktop1:w-[415px] desktop2:w-[485px] flex justify-center">
           {/* <div
@@ -87,6 +87,19 @@ export default function HowItWorksImage() {
           </MotionDivDownToUp>
         </div>
       </SectionWrapper>
+      <div class="custom-shape-divider-bottom-azul mt-[64px] desktop1:mt-[96px]">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
     </SectionArea>
   );
 }
