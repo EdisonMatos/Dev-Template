945b9a7c211d01617dc14d62f125308c6f519e19
Gabriel <gabrielsouza65115@gmail.com>
2024-08-07 11:55:44 -0300

    fix: Ajusta as animações do footer

--- src/components/sections/FooterSocial.jsx
+++ src/components/sections/FooterSocial.jsx
@@ -17,78 +17,80 @@ function FooterSocial() {
       className=" full gap-y-[42px] font-secondFont text-left text-paragraph4 text-lighter flex flex-col py-[26px] phone3:py-[48px] justify-between items-center bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${paralaxeFooter})` }}
     >
-      <MotionDivDownToUp className="w-full">
-        <div className="w-full flex justify-center">
-          <div className=" w-[90%] max-w-[1215px] flex flex-col gap-y-[80px]  desktop1:flex-row desktop1:justify-between">
-            <div className="flex flex-col gap-y-[16px] desktop1:w-[290px] text-paragraph3">
-              <div className="h-[115px] flex items-center">
-                <img
-                  src={content.texts.navbar.logo.img}
-                  alt={content.texts.navbar.logo.alt}
-                  className="w-[60%] tablet1:w-[50%] desktop1:w-[80%]"
-                ></img>
-              </div>
-              <div className="flex full gap-x-[12px] items-center opacity-90">
-                <svg
-                  aria-label="ícone do Whatsapp"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  fill="White"
-                  className="bi bi-whatsapp"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
-                </svg>
-                <a
-                  href={whatsappContactLink}
-                  target="_blank"
-                  className="text-white hover:underline"
-                >
-                  {infos.phone}
-                </a>
-              </div>
-              <div className="flex full gap-x-[12px] items-center opacity-90">
-                <svg
-                  aria-label="ícone do e-mail"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-mail"
-                >
-                  <rect width="20" height="16" x="2" y="4" rx="2" />
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-                </svg>
-                <p className="text-paragraph2">{infos.mail}</p>
-              </div>
-              <div className="flex full gap-x-[12px] items-center opacity-90">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-map-pin"
-                >
-                  <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
-                  <circle cx="12" cy="10" r="3" />
-                </svg>
-                <p>{infos.footerInfos.endereco}</p>
-              </div>
+      <div className=" w-[90%] max-w-[1215px] flex flex-col gap-y-[80px]  desktop1:flex-row desktop1:justify-between">
+        <div className="flex flex-col gap-y-[16px] desktop1:w-[290px] text-paragraph3">
+          <MotionDivDownToUp>
+            <div className="h-[115px] flex items-center">
+              <img
+                src={content.texts.navbar.logo.img}
+                alt={content.texts.navbar.logo.alt}
+                className="w-[60%] tablet1:w-[50%] desktop1:w-[80%]"
+              ></img>
+            </div>
+          </MotionDivDownToUp>
+          <MotionDivDownToUp className="flex flex-col gap-[12px]">
+            <div className="flex full gap-x-[12px] items-center opacity-90">
+              <svg
+                aria-label="ícone do Whatsapp"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="White"
+                className="bi bi-whatsapp"
+                viewBox="0 0 16 16"
+              >
+                <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+              </svg>
+              <a
+                href={whatsappContactLink}
+                target="_blank"
+                className="text-white hover:underline"
+              >
+                {infos.phone}
+              </a>
+            </div>
+            <div className="flex full gap-x-[12px] items-center opacity-90">
+              <svg
+                aria-label="ícone do e-mail"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="white"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-mail"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2" />
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+              </svg>
+              <p className="text-paragraph2">{infos.mail}</p>
+            </div>
+            <div className="flex full gap-x-[12px] items-center opacity-90">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-map-pin"
+              >
+                <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+                <circle cx="12" cy="10" r="3" />
+              </svg>
+              <p>{infos.footerInfos.endereco}</p>
+            </div>
+          </MotionDivDownToUp>
 
-              {/* ENDEREÇO */}
+          {/* ENDEREÇO */}
 
-              {/* <div className="flex full gap-x-[12px] items-center opacity-90">
+          {/* <div className="flex full gap-x-[12px] items-center opacity-90">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
@@ -106,16 +108,17 @@ function FooterSocial() {
               </svg>
               <p>{infos.footerInfos.endereco}</p>
             </div> */}
-            </div>
-            <div className="flex flex-col gap-y-[16px] desktop1:w-[290px]">
-              <h1 className="font-medium text-lighter font-mainFont text-title2 desktop1:text-title5 desktop1:h-[115px] flex items-center">
-                {infos.footerInfos.midSectionName}
-              </h1>
-              {/* Texto footer */}
-              <p className="opacity-90">{infos.footerInfos.footerText}</p>
+        </div>
+        <MotionDivDownToUp>
+          <div className="flex flex-col gap-y-[16px] desktop1:w-[290px]">
+            <h1 className="font-medium text-lighter font-mainFont text-title2 desktop1:text-title5 desktop1:h-[115px] flex items-center">
+              {infos.footerInfos.midSectionName}
+            </h1>
+            {/* Texto footer */}
+            <p className="opacity-90">{infos.footerInfos.footerText}</p>
 
-              {/* Botões de download */}
-              {/* <p className="opacity-90">Baixe nosso app:</p>
+            {/* Botões de download */}
+            {/* <p className="opacity-90">Baixe nosso app:</p>
           <div className="invert">
             {" "}
             <div className="flex flex-col items-start gap-[10px]">
@@ -144,46 +147,46 @@ function FooterSocial() {
             </div>
           </div> */}
 
-              {/* Ícones redes sociais */}
-              <p className="opacity-90">Siga a gente nas redes sociais:</p>
-              <div className="opacity-90">
-                {" "}
-                <div className="flex gap-[10px] items-center">
-                  <a
-                    href={links.socialMedia.instagram}
-                    target="_blank"
-                    aria-label="Link para o Instagram"
-                  >
-                    <IconButton
-                      ariaLabel="Botão para o Instagram"
-                      icon={
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
+            {/* Ícones redes sociais */}
+            <p className="opacity-90">Siga a gente nas redes sociais:</p>
+            <div className="opacity-90">
+              {" "}
+              <div className="flex gap-[10px] items-center">
+                <a
+                  href={links.socialMedia.instagram}
+                  target="_blank"
+                  aria-label="Link para o Instagram"
+                >
+                  <IconButton
+                    ariaLabel="Botão para o Instagram"
+                    icon={
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        fill=""
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="lucide lucide-instagram"
+                      >
+                        <rect
                           width="20"
                           height="20"
-                          viewBox="0 0 24 24"
-                          fill=""
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          class="lucide lucide-instagram"
-                        >
-                          <rect
-                            width="20"
-                            height="20"
-                            x="2"
-                            y="2"
-                            rx="5"
-                            ry="5"
-                          />
-                          <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-                          <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
-                        </svg>
-                      }
-                    />
-                  </a>
-                  {/* <a
+                          x="2"
+                          y="2"
+                          rx="5"
+                          ry="5"
+                        />
+                        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+                        <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+                      </svg>
+                    }
+                  />
+                </a>
+                {/* <a
                 href={links.socialMedia.facebook}
                 target="_blank"
                 aria-label="Link para o Facebook"
@@ -208,7 +211,7 @@ function FooterSocial() {
                   }
                 />
               </a> */}
-                  {/* <a href={links.socialMedia.linkedin} target="_blank">
+                {/* <a href={links.socialMedia.linkedin} target="_blank">
                   <IconButton
                     icon={
                       <svg
@@ -230,83 +233,84 @@ function FooterSocial() {
                     }
                   />
                 </a> */}
-                </div>
               </div>
             </div>
-            <div className="flex flex-col gap-y-[16px] desktop1:w-[290px]">
-              <h1 className="font-medium font-mainFont text-title2 desktop1:text-title5 desktop1:h-[115px] flex items-center text-lighter">
-                NAVEGAÇÃO
-              </h1>
-              <div className="flex justify-between full opacity-90">
-                <div className="w-[46%] flex flex-col gap-y-[16px]">
-                  <div className="h-[36px] hover:underline">
-                    <Link
-                      to="home"
-                      className="cursor-pointer"
-                      spy={true}
-                      smooth={true}
-                      duration={500}
-                      offset={-100}
-                      href="#"
-                    >
-                      <span className="inline-block h-[48px] underline hover:scale-110 transition">
-                        {content.texts.navbar.menuItems[0]}
-                      </span>
-                    </Link>
-                  </div>
-                  <div className="h-[36px] hover:underline">
-                    <Link
-                      to="service"
-                      className="cursor-pointer"
-                      spy={true}
-                      smooth={true}
-                      duration={500}
-                      offset={-50}
-                      href="#"
-                    >
-                      <span className="inline-block h-[48px] underline hover:scale-110 transition">
-                        {content.texts.navbar.menuItems[2]}
-                      </span>
-                    </Link>
-                  </div>
+          </div>
+        </MotionDivDownToUp>
+        <MotionDivDownToUp>
+          <div className="flex flex-col gap-y-[16px] desktop1:w-[290px]">
+            <h1 className="font-medium font-mainFont text-title2 desktop1:text-title5 desktop1:h-[115px] flex items-center text-lighter">
+              NAVEGAÇÃO
+            </h1>
+            <div className="flex justify-between full opacity-90">
+              <div className="w-[46%] flex flex-col gap-y-[16px]">
+                <div className="h-[36px] hover:underline">
+                  <Link
+                    to="home"
+                    className="cursor-pointer"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    offset={-100}
+                    href="#"
+                  >
+                    <span className="inline-block h-[48px] underline hover:scale-110 transition">
+                      {content.texts.navbar.menuItems[0]}
+                    </span>
+                  </Link>
                 </div>
-                <div className=" w-[46%] flex flex-col gap-y-[16px]">
-                  <div className="h-[36px] hover:underline">
-                    <Link
-                      to="about"
-                      className="cursor-pointer"
-                      spy={true}
-                      smooth={true}
-                      duration={500}
-                      offset={-20}
-                      href="#"
-                    >
-                      <span className="inline-block h-[48px] underline hover:scale-110 transition">
-                        {content.texts.navbar.menuItems[1]}
-                      </span>
-                    </Link>
-                  </div>
-                  <div className="h-[36px] hover:underline">
-                    <Link
-                      to="faq"
-                      className="cursor-pointer"
-                      spy={true}
-                      smooth={true}
-                      duration={500}
-                      offset={-50}
-                      href="#"
-                    >
-                      <span className="inline-block h-[48px] underline hover:scale-110 transition">
-                        {content.texts.navbar.menuItems[3]}
-                      </span>
-                    </Link>
-                  </div>
+                <div className="h-[36px] hover:underline">
+                  <Link
+                    to="service"
+                    className="cursor-pointer"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    offset={-50}
+                    href="#"
+                  >
+                    <span className="inline-block h-[48px] underline hover:scale-110 transition">
+                      {content.texts.navbar.menuItems[2]}
+                    </span>
+                  </Link>
+                </div>
+              </div>
+              <div className=" w-[46%] flex flex-col gap-y-[16px]">
+                <div className="h-[36px] hover:underline">
+                  <Link
+                    to="about"
+                    className="cursor-pointer"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    offset={-20}
+                    href="#"
+                  >
+                    <span className="inline-block h-[48px] underline hover:scale-110 transition">
+                      {content.texts.navbar.menuItems[1]}
+                    </span>
+                  </Link>
+                </div>
+                <div className="h-[36px] hover:underline">
+                  <Link
+                    to="faq"
+                    className="cursor-pointer"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                    offset={-50}
+                    href="#"
+                  >
+                    <span className="inline-block h-[48px] underline hover:scale-110 transition">
+                      {content.texts.navbar.menuItems[3]}
+                    </span>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </MotionDivDownToUp>
+        </MotionDivDownToUp>
+      </div>
 
       <div className="hidden w-[88%] max-w-[1110px] h-px bg-secondary"></div>
 
