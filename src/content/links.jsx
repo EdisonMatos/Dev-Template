dcd512f00efabebffb759905faeffe97ef63727c
Gabriel <gabrielsouza65115@gmail.com>
2024-08-08 09:27:16 -0300

    feat: Insere as informações do documento nos links e infos

--- src/content/links.jsx
+++ src/content/links.jsx
@@ -1,10 +1,10 @@
 const links = {
-  whatsapp: "https://wa.me/+55empty?text=",
+  whatsapp: "https://wa.me/+5584996823578?text=",
   ctaWhatsapp:
-    "+55empty?text=Olá! Vim através do site de vocês e gostaria de tirar umas dúvidas.",
-  ctaLink: "#",
+    "+5584996823578?text=Olá! Vim através do site de vocês e gostaria de tirar umas dúvidas.",
+  ctaLink: "84996823578",
   socialMedia: {
-    instagram: "#",
+    instagram: "https://www.instagram.com/euviniciusvictoradv/",
     facebook: "#",
     linkedin: "#",
     twitter: "#",
