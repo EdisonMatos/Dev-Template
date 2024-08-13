8f80243f3cd9194ed172d5191ab2e6df5dfc8982
Gabriel <gabrielsouza65115@gmail.com>
2024-08-13 11:58:27 -0300

    estiliza o texto dos modais e insere um botão no final do conteúdo dentro do modal

--- src/components/sections/FeaturesButton.jsx
+++ src/components/sections/FeaturesButton.jsx
@@ -4,10 +4,14 @@ import SectionArea from "../sectionElements/SectionArea";
 import SectionHeader from "../sectionElements/SectionHeader";
 import SectionWrapper from "../sectionElements/SectionWrapper";
 import content from "../../content/content";
+import Button from "../interactives/Button";
+import links from "../../content/links";
 import { Dialog } from "primereact/dialog";
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+
+const whatsappContactLink = `https://wa.me/` + `${links.ctaWhatsapp}`;
 
 export default function FeaturesButton() {
   const [visible, setVisible] = useState(false);
@@ -44,15 +48,38 @@ export default function FeaturesButton() {
               buttonLabel={content.texts.features.card1.buttonLabel}
               onClick={() =>
                 onClick(
-                  <p>
-                    Oferece uma formação prática e teórica sobre os princípios
-                    contábeis e práticas financeiras essenciais para uma gestão
-                    eficaz. Os participantes aprenderão a analisar balanços,
-                    elaborar relatórios financeiros e desenvolver estratégias
-                    para otimização de recursos, com foco na melhoria da saúde
-                    financeira e no suporte à tomada de decisões gerenciais em
-                    ambientes corporativos e empresariais.
-                  </p>
+                  <div>
+                    <p className="font-normal">
+                      Oferece uma formação prática e teórica sobre os princípios
+                      contábeis e práticas financeiras essenciais para uma
+                      gestão eficaz. Os participantes aprenderão a analisar
+                      balanços, elaborar relatórios financeiros e desenvolver
+                      estratégias para otimização de recursos, com foco na
+                      melhoria da saúde financeira e no suporte à tomada de
+                      decisões gerenciais em ambientes corporativos e
+                      empresariais.
+                    </p>
+                    <div className="flex justify-start w-full mt-[16px]">
+                      <Button
+                        aria-label={content.texts.hero.ctaButtonAriaLabel}
+                        label={content.texts.hero.ctaButtonText}
+                        buttonLink={whatsappContactLink}
+                        className="w-[100%]"
+                        icon={
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="currentColor"
+                            className="bi bi-whatsapp"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+                          </svg>
+                        }
+                      />
+                    </div>
+                  </div>
                 )
               }
             />
@@ -63,13 +90,35 @@ export default function FeaturesButton() {
               buttonLabel={content.texts.features.card2.buttonLabel}
               onClick={() =>
                 onClick(
-                  <p>
-                    Torne-se um especialista em Recursos Humanos e Departamento
-                    Pessoal! Aprenda sobre os direitos e deveres dos
-                    funcionários de acordo com a CLT. Inscreva-se no nosso curso
-                    para iniciantes e comece a sua jornada na área de RH e DP.
-                    Venha conhecer mais sobre o nosso curso!
-                  </p>
+                  <div>
+                    <p className="font-normal">
+                      Torne-se um especialista em Recursos Humanos e
+                      Departamento Pessoal! Aprenda sobre os direitos e deveres
+                      dos funcionários de acordo com a CLT. Inscreva-se no nosso
+                      curso para iniciantes e comece a sua jornada na área de RH
+                      e DP. Venha conhecer mais sobre o nosso curso!
+                    </p>
+                    <div className="flex justify-start w-full mt-[16px]">
+                      <Button
+                        aria-label={content.texts.hero.ctaButtonAriaLabel}
+                        label={content.texts.hero.ctaButtonText}
+                        buttonLink={whatsappContactLink}
+                        className="w-[100%]"
+                        icon={
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="currentColor"
+                            className="bi bi-whatsapp"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+                          </svg>
+                        }
+                      />
+                    </div>
+                  </div>
                 )
               }
             />
@@ -80,13 +129,35 @@ export default function FeaturesButton() {
               buttonLabel={content.texts.features.card3.buttonLabel}
               onClick={() =>
                 onClick(
-                  <p>
-                    Descubra como nossa consultoria pode ajudar você a gerenciar
-                    DRE, balanço patrimonial, apuração de tributos, e muito
-                    mais. Com nossa orientação, você estará pronto para otimizar
-                    suas finanças e tomar decisões informadas para o crescimento
-                    sustentável da sua empresa!
-                  </p>
+                  <div>
+                    <p className="font-normal">
+                      Descubra como nossa consultoria pode ajudar você a
+                      gerenciar DRE, balanço patrimonial, apuração de tributos,
+                      e muito mais. Com nossa orientação, você estará pronto
+                      para otimizar suas finanças e tomar decisões informadas
+                      para o crescimento sustentável da sua empresa!
+                    </p>
+                    <div className="flex justify-start w-full mt-[16px]">
+                      <Button
+                        aria-label={content.texts.hero.ctaButtonAriaLabel}
+                        label={content.texts.hero.ctaButtonText}
+                        buttonLink={whatsappContactLink}
+                        className="w-[100%]"
+                        icon={
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="currentColor"
+                            className="bi bi-whatsapp"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+                          </svg>
+                        }
+                      />
+                    </div>
+                  </div>
                 )
               }
             />
@@ -97,22 +168,44 @@ export default function FeaturesButton() {
               buttonLabel={content.texts.features.card4.buttonLabel}
               onClick={() =>
                 onClick(
-                  <p>
-                    CRONOGRAMA do curso de Demonstração do Resultado no
-                    Exercício: <br />
-                    - Conceito DRE <br />
-                    - O que é e para que serve DRE - Conceito de Markup <br />
-                    - O que é Markup? <br />
-                    - O que é faturamento? <br />
-                    - O que é receita? O que é lucro? <br />
-                    - Planos de contas O que é plano de contas?
-                    <br />
-                    - Como estruturar o plano de contas? <br />
-                    - Calcular DRE <br />
-                    - Estrutura do DRE <br />
-                    - Como fazer o DRE? <br />- Entender e calcular o DRE -
-                    Saber interpretar o DRE
-                  </p>
+                  <div>
+                    <p className="font-normal">
+                      CRONOGRAMA do curso de Demonstração do Resultado no
+                      Exercício: <br />
+                      - Conceito DRE <br />
+                      - O que é e para que serve DRE - Conceito de Markup <br />
+                      - O que é Markup? <br />
+                      - O que é faturamento? <br />
+                      - O que é receita? O que é lucro? <br />
+                      - Planos de contas O que é plano de contas?
+                      <br />
+                      - Como estruturar o plano de contas? <br />
+                      - Calcular DRE <br />
+                      - Estrutura do DRE <br />
+                      - Como fazer o DRE? <br />- Entender e calcular o DRE -
+                      Saber interpretar o DRE
+                    </p>
+                    <div className="flex justify-start w-full mt-[16px]">
+                      <Button
+                        aria-label={content.texts.hero.ctaButtonAriaLabel}
+                        label={content.texts.hero.ctaButtonText}
+                        buttonLink={whatsappContactLink}
+                        className="w-[100%]"
+                        icon={
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            fill="currentColor"
+                            className="bi bi-whatsapp"
+                            viewBox="0 0 16 16"
+                          >
+                            <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232" />
+                          </svg>
+                        }
+                      />
+                    </div>
+                  </div>
                 )
               }
             />
