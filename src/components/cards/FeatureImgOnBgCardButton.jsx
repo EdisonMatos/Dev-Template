0e47fc5e144aa283f28cbd3cdfcfd0656445c8ad
Gabriel <gabrielsouza65115@gmail.com>
2024-08-19 15:01:08 -0300

    fix: Corrige a animação nos elementos dentro do Componente FeatureImgOnBgCardButton

--- src/components/cards/FeatureImgOnBgCardButton.jsx
+++ src/components/cards/FeatureImgOnBgCardButton.jsx
@@ -42,31 +42,33 @@ export default function FeatureImgOnBgCardButton(props) {
               {description}
             </p>
             <div>
-              <Button
-                label={buttonLabel}
-                buttonLink={buttonLink}
-                color={buttonColor}
-                className="text-white"
-                size="small"
-                onClick={onClick}
-                icon={
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="lucide lucide-corner-down-right"
-                  >
-                    <polyline points="15 10 20 15 15 20" />
-                    <path d="M4 4v7a4 4 0 0 0 4 4h12" />
-                  </svg>
-                }
-              />
+              <Animation>
+                <Button
+                  label={buttonLabel}
+                  buttonLink={buttonLink}
+                  color={buttonColor}
+                  className="text-white"
+                  size="small"
+                  onClick={onClick}
+                  icon={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="lucide lucide-corner-down-right"
+                    >
+                      <polyline points="15 10 20 15 15 20" />
+                      <path d="M4 4v7a4 4 0 0 0 4 4h12" />
+                    </svg>
+                  }
+                />
+              </Animation>
             </div>
           </Animation>
         </div>
