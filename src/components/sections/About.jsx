74e1f589f2a4a7cf21200238cccc581ccaf691d4
Edison Matos <edison.matos@live.com>
2024-08-23 14:50:21 -0300

    feat: Resolve animação do about

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -92,6 +92,35 @@ export default function About() {
             <Paragraphs className="text-white text-opacity-80">
               <AboutFading />
             </Paragraphs>
+            {/* Início Botão de saiba mais abrindo modal */}
+
+            <Button
+              className="mt-[48px]"
+              label="Continuar lendo"
+              onClick={onClick}
+              removeAnchor={true}
+              removeTarget={true}
+              animation={false}
+              icon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="lucide lucide-move-right"
+                >
+                  <path d="M18 8L22 12L18 16" />
+                  <path d="M2 12H22" />
+                </svg>
+              }
+            />
+
+            {/* Fim Botão de saiba mais abrindo modal */}
           </MotionDivDownToUp>
 
           {/* Início Botões das redes sociais */}
@@ -179,37 +208,6 @@ export default function About() {
           </div> */}
 
           {/* Fim Botões das redes sociais */}
-
-          {/* Início Botão de saiba mais abrindo modal */}
-
-          <MotionDivDownToUp>
-            <Button
-              className="mt-[48px]"
-              label="Continuar lendo"
-              onClick={onClick}
-              removeAnchor={true}
-              removeTarget={true}
-              icon={
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="lucide lucide-move-right"
-                >
-                  <path d="M18 8L22 12L18 16" />
-                  <path d="M2 12H22" />
-                </svg>
-              }
-            />
-          </MotionDivDownToUp>
-
-          {/* Fim Botão de saiba mais abrindo modal */}
         </div>
       </SectionWrapper>
       <Dialog
