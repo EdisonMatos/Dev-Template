f5202d9d8acf8b00bd33507e7798120fea92751e
Edison Matos <edison.matos@live.com>
2024-09-17 14:32:20 -0300

    feat: Insere animações

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -7,6 +7,9 @@ import baloonFiesta from "../assets/imgs/hero/baloonFiesta.png";
 import cakeFiesta from "../assets/imgs/hero/cakeFiesta.png";
 import giftsFiesta from "../assets/imgs/hero/giftsFiesta.png";
 import masksFiesta from "../assets/imgs/hero/masksFiesta.png";
+import MotionDivUpToDown from "../components/animation/MotionDivUpToDown";
+import MotionDivDownToUp from "../components/animation/MotionDivDownToUp";
+import "../styles/fiesta.css";
 
 export default function Index() {
   return (
@@ -15,18 +18,44 @@ export default function Index() {
       style={{ backgroundImage: `url(${bgHeroImg})` }}
     >
       <SectionArea className=" h-screen flex items-center justify-center">
-        <SectionWrapper className="bg-green-500 relative">
-          <div className="logoAndButtonArea bg-yellow-800 w-[60%] flex-col items-center justify-center my-[40px]">
+        <SectionWrapper className=" relative">
+          <img
+            src={baloonFiesta}
+            alt="Imagem de balão"
+            className="w-[25%] desktop1:w-[25%] absolute left-0 top-0 fadeinfiest"
+          />
+
+          <img
+            src={cakeFiesta}
+            alt="Imagem de balão"
+            className="w-[25%] desktop1:w-[25%] absolute left-0 bottom-0 fadeinfiest"
+          />
+          <img
+            src={masksFiesta}
+            alt="Imagem de balão"
+            className="w-[25%] desktop1:w-[25%] absolute right-0 top-0 fadeinfiest"
+          />
+          <img
+            src={giftsFiesta}
+            alt="Imagem de balão"
+            className="w-[25%] desktop1:w-[25%] absolute right-0 bottom-0 fadeinfiest"
+          />
+
+          <div className="logoAndButtonArea w-[100%] desktop1:w-[60%] flex-col items-center justify-center my-[40px] ">
             <div className="mb-[20px]">
-              <img src={imgLogoFiesta} alt="Logo Cash Fiesta" />
+              <MotionDivUpToDown>
+                <img src={imgLogoFiesta} alt="Logo Cash Fiesta" />
+              </MotionDivUpToDown>
             </div>
-            <div className="bg-green-800 flex items-center">
-              <a href="#" className="bg-red-500 flex justify-center">
-                <img
-                  src={imgButtonFiesta}
-                  alt="Logo Button Fiesta"
-                  className="w-[50%] hover:scale-110 transition"
-                />
+            <div className="flex items-center ">
+              <a href="#" className="w-full">
+                <MotionDivDownToUp className="flex justify-center w-full">
+                  <img
+                    src={imgButtonFiesta}
+                    alt="Logo Button Fiesta"
+                    className="w-[40%] pulse-button"
+                  />
+                </MotionDivDownToUp>
               </a>
             </div>
           </div>
