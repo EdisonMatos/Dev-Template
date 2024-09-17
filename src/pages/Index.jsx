4cd1031ba9e2e2ca7c4323960d2b661e90b1cf7a
Edison Matos <edison.matos@live.com>
2024-09-17 11:31:49 -0300

    feat: Cria áreas de telas

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,36 +1,16 @@
-import Faq from "../components/sections/Faq";
-import About from "../components/sections/About";
-import BackToTopButton from "../components/interactives/BackToTopButton";
-import HeroBgImg from "../components/sections/HeroBgImg";
-import FooterSocial from "../components/sections/FooterSocial";
-import NavbarSocial from "../components/sections/NavbarSocial";
-import FeaturesWithIcons from "../components/sections/FeaturesWithIcons";
-import Numbers from "../components/sections/Numbers";
-import AboutInstagram from "../components/sections/AboutInstagram";
-import HowItWorksImage from "../components/sections/HowItWorksImage";
-import CtaMinimal from "../components/sections/CtaMinimal";
-import Depositions from "../components/sections/Depositions";
-import Hero from "../components/sections/Hero";
-import FeaturesButton from "../components/sections/FeaturesButton";
-import FloatingWhatsappButton from "../components/interactives/FloatingWhatsappButton";
+import bgHeroImg from "../assets/imgs/hero/bgHero.jpg";
+import SectionArea from "../components/sectionElements/SectionArea";
+import SectionWrapper from "../components/sectionElements/SectionWrapper";
 
 export default function Index() {
   return (
-    <div>
-      <NavbarSocial />
-      <Hero />
-      <FeaturesWithIcons />
-      {/* <FeaturesButton /> */}
-      {/* <Numbers /> */}
-      <About />
-      {/* <AboutInstagram /> */}
-      <CtaMinimal />
-      {/* <Depositions /> */}
-      <HowItWorksImage />
-      <Faq />
-      <FooterSocial />
-      <FloatingWhatsappButton />
-      <BackToTopButton />
+    <div
+      className="bg-cover h-screen w-screen"
+      style={{ backgroundImage: `url(${bgHeroImg})` }}
+    >
+      <SectionArea className="bg-red-500 h-screen flex items-center justify-center">
+        <SectionWrapper className="bg-green-500">a</SectionWrapper>
+      </SectionArea>
     </div>
   );
 }
