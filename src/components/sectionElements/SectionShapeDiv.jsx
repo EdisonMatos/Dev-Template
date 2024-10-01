7cc75276fa913824c680fff052cfb1542d5a86f2
Gabriel <gabrielsouza65115@gmail.com>
2024-10-01 14:15:21 -0300

    fix: Adiciona propriedade para inverter svg

--- src/components/sectionElements/SectionShapeDiv.jsx
+++ src/components/sectionElements/SectionShapeDiv.jsx
@@ -21,10 +21,12 @@ SectionShapeDiv.propTypes = {
   shapeDivBook: PropTypes.bool,
   paddingtop: PropTypes.bool,
   paddingbot: PropTypes.bool,
+  rotateShape: PropTypes.bool,
 };
 
 SectionShapeDiv.defaultProps = {
   shapeDiv: true,
+  rotateShape: false,
 };
 
 export default function SectionShapeDiv({
@@ -43,9 +45,11 @@ export default function SectionShapeDiv({
   shapeColor,
   paddingtop = false,
   paddingbot = false,
+  rotateShape = false,
 }) {
   const paddingTopp = paddingtop ? "pt-[64px] desktop1:pt-[96px]" : "";
   const paddingBottom = paddingbot ? "pb-[64px] desktop1:pb-[96px]" : "";
+  const rotationClass = rotateShape ? "rotate-180" : "";
 
   return (
     shapeDiv && (
@@ -59,6 +63,7 @@ export default function SectionShapeDiv({
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 1200 120"
               preserveAspectRatio="none"
+              className={`${rotationClass}`}
             >
               <path
                 d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
