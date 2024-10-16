22aa8a61cae3afddef7e598fff1717d26dd36862
Gabriel <gabrielsouza65115@gmail.com>
2024-10-16 11:24:18 -0300

    fix: Corrige tamanho da logo e bitão Hambúrguer

--- src/components/sections/NavbarSocial.jsx
+++ src/components/sections/NavbarSocial.jsx
@@ -107,11 +107,11 @@ export default function NavbarSocial({ LightMode }) {
               className={`${
                 LightMode
                   ? scrolling
-                    ? "bg-transparent"
-                    : "bg-white px-3 py-3 transition-all duration-500 ease-in-out"
+                    ? "bg-transparent w-[50%] tablet1:w-[50%] tablet2:w-[40%] desktop1:w-[60%] desktop2:w-[60%] transition-all duration-1000"
+                    : "bg-white w-[64%] my-[20px] tablet1:w-[55%] tablet2:w-[47%] desktop1:w-[70%] desktop2:w-[100%] px-3 py-3 transition-all duration-1000"
                   : scrolling
-                  ? "w-[50%] max-w-[50%] tablet2:w-[40%] tablet2:max-w-[40%] desktop1:w-[60%] desktop1:max-w-[60%] transition-all duration-1000 "
-                  : "my-[20px] w-[70%] tablet1:w-[60%] tablet2:w-[50%] desktop1:w-[70%] desktop2:w-[100%] transition-all duration-1000 "
+                  ? "bg-white w-[50%] tablet1:w-[50%] tablet2:w-[40%] desktop1:w-[60%] desktop2:w-[60%] transition-all duration-1000"
+                  : "bg-white w-[64%] my-[20px] tablet1:w-[55%] tablet2:w-[47%] desktop1:w-[70%] desktop2:w-[100%] transition-all duration-1000"
               } tablet3:mb-0`}
             />
           </ScrollLink>
