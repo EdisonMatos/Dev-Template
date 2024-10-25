90459d6376b1f1295a2c404829370b07778057b8
Gabriel <gabrielsouza65115@gmail.com>
2024-10-25 10:36:08 -0300

    fix: Ajusta a posição do texto do footer

--- src/components/sections/FooterSocial.jsx
+++ src/components/sections/FooterSocial.jsx
@@ -30,7 +30,7 @@ const observationInfo = {
 function FooterSocial({ LightMode }) {
   return (
     <footer className="bg-bgSectionDark full gap-y-[42px] text-white font-secondFont text-left text-paragraph4 flex flex-col justify-between items-center bg-cover bg-center bg-no-repeat">
-      <SectionArea paddingtop={true} paddingbot={false}>
+      <SectionArea paddingtop={true} >
         {/* <SectionShapeDiv shapeDivArrow shapeColor="text-neutral-100" /> */}
         <SectionWrapper>
           <div className=" w-full max-w-[1215px] flex flex-col gap-y-[80px] desktop1:flex-row desktop1:justify-between">
@@ -328,7 +328,7 @@ function FooterSocial({ LightMode }) {
 
           <div className="hidden w-[88%] max-w-[1110px] bg-secondary"></div>
 
-          <div className="text-center w-[88%] max-w-[1110px] flex flex-col mt-[60px] mb-[93px] phone2:mb-[80px] tablet1:mb-[40px] desktop1:mb-[30px]">
+          <div className="text-center w-[88%] max-w-[1110px] flex flex-col mt-[60px] mb-[48px] phone2:mb-[80px] tablet1:mb-0">
             <p className="w-full opacity-75">
               {content.texts.footer.copyrightLine}
             </p>
