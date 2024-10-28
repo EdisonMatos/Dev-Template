35e5482974176f1560c734f870aeeb92dec4787e
Edison Matos <edison.matos@live.com>
2024-10-28 17:09:59 -0300

    feat: Finaliza hero

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -56,23 +56,9 @@ export default function Hero() {
                 </div>
                 <div className="flex justify-center desktop1:justify-start">
                   <div className="flex items-center">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="#dbb14c"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      class="lucide lucide-map-pin"
-                    >
-                      <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
-                      <circle cx="12" cy="10" r="3" />
-                    </svg>
+                    {content.texts.hero.obsHero.icon}
                     <p className="text-white opacity-80 ml-[10px] text-center">
-                      Atendimento presencial ou online
+                      {content.texts.hero.obsHero.text}
                     </p>
                   </div>
                 </div>
--- src/content/content.jsx
+++ src/content/content.jsx
@@ -121,6 +121,26 @@ const content = {
         "Com expertise em inventários e regularização de imóveis, garantimos que seu patrimônio seja protegido e seus direitos plenamente assegurados, do início ao fim do processo.",
       heroImg: heroImg,
       ctaButtonText: "Entre em contato",
+      obsHero: {
+        icon: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#dbb14c"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-map-pin"
+          >
+            <path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+            <circle cx="12" cy="10" r="3" />
+          </svg>
+        ),
+        text: "Atendimento presencial ou online",
+      },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
       secondaryCta: "A Definir",
