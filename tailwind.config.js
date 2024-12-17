bc7eaf22d024171c7560319b6d8d4105b6efc6f1
Edison Matos <edison.matos@live.com>
2024-12-17 17:28:24 -0300

    feat: Termina features

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
   extend: {
     colors: {
-      primary: "#AF8B70",
+      primary: "#CDBAAC",
       secondary: "#000000",
       bgSectionLight: "#FFFFFF",
       bgSectionDark: "#2e394b",
