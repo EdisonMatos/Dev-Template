4bee265c7b611f0411e479e0b438ef65c45d8d09
Gabriel <gabrielsouza65115@gmail.com>
2025-01-10 15:50:14 -0300

    fix: ajeita a responsividade do balão do whatsChat

--- src/components/interactives/WhatsChat.jsx
+++ src/components/interactives/WhatsChat.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FloatingWhatsApp } from "@carlos8a/react-whatsapp-floating-button";
 import imgProfilePicture from "../../assets/favico/android-icon-192x192.png";
 import "../../../src/index.css";
@@ -7,23 +7,40 @@ import { infos } from "../../content/content";
 const whatsNumber = `${infos.phone.ddd}${infos.phone.firstPart}${infos.phone.secondPart}`;
 
 export default function WhatsChat() {
-  const [chatVisible, setChatVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(false); // Controla a visibilidade do chat
+  const tooltipTextRef = useRef(null); // Referência para o texto da tooltip
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        document.documentElement.scrollTop || document.body.scrollTop;
+
+      // Torna o chat visível se o usuário rolar mais de 100px
+      setIsVisible(scrollTop > 100);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const handleCloseChat = () => {
     // Oculta o chat e o exibe novamente após um curto intervalo
-    setChatVisible(false);
-    setTimeout(() => setChatVisible(true), 100); // Ajuste o tempo conforme necessário
+    setIsVisible(false);
+    setTimeout(() => setIsVisible(true), 100); // Ajuste o tempo conforme necessário
   };
 
   return (
     <div>
-      {chatVisible && (
+      {isVisible && ( // Exibe o chat apenas se isVisible for true
         <FloatingWhatsApp
           status="w-3"
           chatboxHeight="auto"
           phoneNumber={whatsNumber}
           notification={true}
-          notificationDelay={5}
+          notificationDelay={2}
           notificationLoop={2}
           accountName={infos.name}
           avatar={imgProfilePicture}
@@ -32,8 +49,10 @@ export default function WhatsChat() {
           statusMessage="Disponível"
           startChatText="Falar no whatsapp"
           tooltipText={
-            <p className="text-paragraph4">
-              Disponível <strong>24h</strong> ashduahds
+            <p ref={tooltipTextRef} className="">
+              Disponível <strong>24h</strong>
+              Disponível Disponível Disponível Disponível Disponível Disponível
+              Disponível Disponível Disponível jshjsdhdjs dsdhsdhdshk
             </p>
           }
           allowEsc={true}
