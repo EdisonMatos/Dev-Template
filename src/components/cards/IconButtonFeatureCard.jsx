f0d4e5b2834806770b6ccf83a8a6e1e2aa3db94d
Edison Matos <edison.matos@live.com>
2025-01-13 15:10:29 -0300

    feat: Corrige tamanho de card de features em versão desktop

--- src/components/cards/IconButtonFeatureCard.jsx
+++ src/components/cards/IconButtonFeatureCard.jsx
@@ -3,7 +3,7 @@ export default function IconButtonFeatureCard(props) {
 
   return (
     <div
-      className={`w-full p-0 tablet1:w-[200px] desktop1:w-[220px] flex flex-col items-center  desktop1:p-0 ${className}`}
+      className={`w-full p-0 tablet1:w-[200px] desktop1:w-[240px] flex flex-col items-center  desktop1:p-0 ${className}`}
     >
       <div className="h-[64px] w-[64px] mb-[24px]  rounded-md flex justify-center items-center text-bgSectionDark bg-primary">
         {icon}
