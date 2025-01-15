8955c9fcad2c4f8fec8e38f479fce36f0235ffd6
Gabriel <gabrielsouza65115@gmail.com>
2025-01-14 22:56:40 -0300

    fix: Retira a visibilidade do componente FloatingWhatsChat para melhor visualização do WhatsChat

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -54,7 +54,7 @@ export default function Index() {
         x={false}
       />
       {/* <FloatingWhatsappButton /> */}
-      <BackToTopButton />
+      {/* <BackToTopButton /> */}
       <WhatsChat />
     </div>
   );
