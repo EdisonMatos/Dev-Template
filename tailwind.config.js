7a0a15e520a2387c272c110297416ec2735e8e19
Edison Matos <edison.matos@live.com>
2025-01-31 15:33:07 -0300

    feat: Adiciona favico e imagens

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
   extend: {
     colors: {
-      primary: "#B4A185",
+      primary: "#F5E6C7",
       secondary: "#000000",
       bgSectionLight: "#FFFFFF",
       bgSectionDark: "#151515",
