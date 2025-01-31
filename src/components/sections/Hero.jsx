3cbc4d4676a55c98132c7c96b19823a3055c2095
Gabriel <gabrielsouza65115@gmail.com>
2025-01-30 21:28:51 -0300

    feat: Insere as informações na Hero de acordo com o documento

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -50,7 +50,7 @@ export default function Hero() {
                   <div className="flex justify-center desktop1:justify-start">
                     <div className="flex items-center text-primary">
                       {content.texts.hero.obsHero.icon}
-                      <p className="text-white opacity-80 ml-[10px] text-center">
+                      <p className="text-white opacity-80 ml-[10px] text-start">
                         {content.texts.hero.obsHero.text}
                       </p>
                     </div>
--- src/content/content.jsx
+++ src/content/content.jsx
@@ -94,14 +94,14 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag: "ADVOGADO CÍVEL E TRABALHISTA EM CAMPINAS - SP",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Mais de uma década defendendo
+          <span className="text-primary"> seus direitos</span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle: "Com experiência e dedicação, garanto justiça em cada caso, lutando sempre pelo que é seu por direito.",
       heroImg: heroImg,
       ctaButtonText: "Entre em contato",
       obsHero: {
@@ -123,7 +123,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "Atendimento presencial e online",
+        text: "Atendimento presencial em Campinas - SP e online para todo Brasil",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
