6c2f5b0d8f8424eefc20993f32a61379da6af652
Edison Matos <edison.matos@live.com>
2025-02-13 18:03:56 -0300

    feat: Atualiza em outros lugares

--- src/components/interactives/BackToTopButton.jsx
+++ src/components/interactives/BackToTopButton.jsx
@@ -33,7 +33,7 @@ const BackToTopButton = () => {
       } fixed bottom-8 left-8 p-4 bg-primary text-black rounded-full z-10 focus:outline-none hover:scale-125 transition`}
       onClick={scrollToTop}
     >
-      <FaArrowUp className="text-bgSectionDark" />
+      <FaArrowUp className="text-labelsIcons" />
     </button>
   );
 };
--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -18,7 +18,7 @@ export default function Hero({ appDownloadButtons }) {
             <div className="flex  flex-col w-full desktop1:w-[50%] desktop1:mr-[20px]">
               <MotionDivDownToUp>
                 <div className="flex justify-center w-auto text-center desktop1:justify-start desktop1:text-left font-secondFont text-paragraph4">
-                  <p className="mb-[16px] bg-primary bg-opacity-100 rounded-md px-[16px] py-[6px] inline-block text-paragraph2">
+                  <p className="mb-[16px] bg-primary text-labelsIcons bg-opacity-100 rounded-md px-[16px] py-[6px] inline-block text-paragraph2">
                     {content.texts.hero.miniTag}
                   </p>
                 </div>
