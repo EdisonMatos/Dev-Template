84c79bb6ae7cb885eaedb2bbd96d50ef7148ec40
Edison Matos <edison.matos@live.com>
2025-02-17 17:28:37 -0300

    feat: Finaliza cores de roberta zani

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
   extend: {
     colors: {
-      primary: "#B4A185",
+      primary: "#DBB14C",
       secondary: "#000000",
       bgSectionLight: "#FFFFFF",
       bgSectionDark: "#252525",
