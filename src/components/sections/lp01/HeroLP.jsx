776bd7d0b32ce006dbc9c29fa5d683a7376449d7
Gabriel <gabrielsouza65115@gmail.com>
2025-02-19 09:55:15 -0300

    fix: Corrige espaçamento de seção em tela 1024px

--- src/components/sections/lp01/HeroLP.jsx
+++ src/components/sections/lp01/HeroLP.jsx
@@ -75,7 +75,7 @@ export default function HeroLP({ fullImg }) {
       </div>
       {/*-------- Versão para tablet1 pra frente -------- */}
       <div
-        className="hidden tablet1:flex bg-top bg-[#EEA935] font-mainFont bg-[size:100%_auto] phone2:bg-[size:100%_auto] phone3:bg-[size:100%_auto] tablet1:bg-cover bg-no-repeat h-[100vh]"
+        className="hidden tablet1:flex bg-top bg-[#EEA935] font-mainFont bg-[size:100%_auto] phone2:bg-[size:100%_auto] phone3:bg-[size:100%_auto] tablet1:bg-cover bg-no-repeat h-auto"
         style={{
           backgroundImage: `url(${contentLp01.hero.bgImg})`,
           backgroundPosition: "top",
