b5ef52cd34eaee4da178695acd1af254796a7170
Gabriel <gabrielsouza65115@gmail.com>
2025-02-25 18:56:48 -0300

    feat: Insere os textos da Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -94,14 +94,14 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag: "ADVOGADO PREVIDENCIARISTA EM TERESINA - PI",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Atendimento personalizado e acessibilidade total
+          <span className="text-primary">  ao seu caso</span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle: "Atentos às mudanças legislativas, cuidamos de toda burocracia para você garantir seus direitos previdenciários.",
       heroImg: heroImg,
       ctaButtonText: "Entre em contato",
       obsHero: {
@@ -123,7 +123,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "Atendimento presencial e online",
+        text: "Atendimento presencial sob consulta e online",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
