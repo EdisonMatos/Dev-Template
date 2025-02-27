3864eac6750453072a416051e8e9b5083f9ae719
Gabriel <gabrielsouza65115@gmail.com>
2025-02-27 14:22:07 -0300

    fix: Insere as redes sociais do doc

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -67,8 +67,8 @@ export const infos = {
     </div>
   ),
   obs: <span className="italic">A Definir</span>,
-  instagramProfile: "A_definir", // Sem o @
-  facebookProfile: "A_Definir",
+  instagramProfile: "agnescristina.adv", // Sem o @
+  facebookProfile: "resenhadedireito3",
   linkeDinProfile: "A_Definir",
   x: "A_Definir",
   whatsappDefaultMessage:
