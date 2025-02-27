baacbe0fce46785254ca60a66e83e3561723020d
Gabriel <gabrielsouza65115@gmail.com>
2025-02-27 11:26:54 -0300

    fix: Atualiza a cor no tailwind

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -42,7 +42,7 @@ export default function Index() {
         addres={true}
         instagram={true}
         facebook={true}
-        linkedin={false}
+        linkedin={false}c
         x={false}
         obs={false}
       />
