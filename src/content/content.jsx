ac9009aa42048c727738a01776042e359cd23a29
Gabriel <gabrielsouza65115@gmail.com>
2025-03-03 13:18:41 -0300

    fix: Corrige o texto do botão social no aboutSocial

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -472,8 +472,8 @@ const content = {
         miniTag: "REDES SOCIAIS",
         title: "Conecte-se conosco",
         subtitle: "Aproveite nossas redes sociais para mantermos contato e ficar por dentro de atualizações importantes no mundo do direito.",
-        labelInstagram: "Siga-nos no Instagram",
-        labelFacebook: "Siga-nos no Facebook",
+        labelInstagram: "Siga-me no Instagram",
+        labelFacebook: "Siga-me no Facebook",
         labelLinkedin: "Siga-nos no LinkedIn",
         labelX: "Siga-nos no X",
         paragraph: (
