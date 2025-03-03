491d3a730b7da07c5ae3177690b33df938f90219
Gabriel <gabrielsouza65115@gmail.com>
2025-03-03 12:21:50 -0300

    fix: Preenche os textos da Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -94,14 +94,14 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag: "ADVOGADA EM CUIABÁ - MT",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Blindagem jurídica
+          <span className="text-primary"> para sua carreira médica</span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle: "Especialista na área, ofereço suporte jurídico estratégico para garantir sua segurança profissional.",
       heroImg: heroImg,
       ctaButtonText: "Entre em contato",
       obsHero: {
@@ -123,7 +123,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "Atendimento presencial e online",
+        text: "Atendimento online para todo Brasil",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
