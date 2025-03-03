9a5c73416bad159d2b0fbb53e81678dcc5b1c114
Edison Matos <edison.matos@live.com>
2025-03-03 07:14:45 -0300

    feat: Finaliza config de blog antes da estilização

--- src/components/sections/BlogPosts.jsx
+++ src/components/sections/BlogPosts.jsx
@@ -29,7 +29,7 @@ function BlogPosts() {
             color=""
             type=""
           />
-          <ul className="flex flex-wrap gap-[30px]  justify-center mb-[26px] tablet1:mb-[40px] desktop1:mb-[80px]">
+          <ul className="flex flex-wrap gap-[30px]  justify-center mb-[80px]">
             {posts.slice(0, 3).map((post) => (
               <li key={post.ID}>
                 <WordPressBlogCard
