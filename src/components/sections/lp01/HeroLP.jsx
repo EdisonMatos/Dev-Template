414b9463b9297937336985761089ffc6e9353580
Gabriel <gabrielsouza65115@gmail.com>
2025-03-05 13:59:08 -0300

    fix: Insere imagem na Lpv com background da cor do fundo

--- src/components/sections/lp01/HeroLP.jsx
+++ src/components/sections/lp01/HeroLP.jsx
@@ -8,7 +8,7 @@ export default function HeroLP({ fullImg }) {
   return fullImg ? (
     <div className="gradient-LP-Hero">
       <div
-        className="desktop1:hidden font-mainFont bg-[size:100%_auto] phone2:bg-[size:100%_auto] phone3:bg-[size:100%_auto]  bg-no-repeat h-auto"
+        className="desktop1:hidden font-mainFont bg-[size:100%_auto] phone2:bg-[size:100%_auto] phone3:bg-[size:100%_auto] bg-no-repeat h-auto"
         style={{
           backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)), url(${contentLp01.hero.bgImg})`,
         }}
