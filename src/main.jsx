8e53c92b21dc743b0fa45ef736b27db3a1a49939
Gabriel de Souza da Silva <biell@MacBook-Air-de-Gabriel.local>
2025-03-10 08:44:03 -0300

    fix: Insere propriedade que deve ser escolhida no (main.jsx) para optar entre número de Whatsapp ou formulário

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       {/* <Route path="/" element={<Lp01 />} /> */}
       {/* <Route path="/lp02" element={<Lp02 />} /> */}
       {/* <Route path="/lp03" element={<Lp03 />} /> */}
-      <Route path="/whatsapp" element={<WhatsAppLinks />} />
+      <Route path="/whatsapp" element={<WhatsAppLinks form={true} />} />
     </Routes>
   </Router>
 );
