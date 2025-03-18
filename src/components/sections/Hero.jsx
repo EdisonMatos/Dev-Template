1caff5b4d120576aadd308b6b10d2e9dbd26eb28
Gabriel de Souza da Silva <biell@MacBook-Air-de-Gabriel.local>
2025-03-18 11:47:00 -0300

    fix: Atualiza a Feature

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -112,7 +112,7 @@ export default function Hero({ appDownloadButtons, personImg, fundoImage }) {
         </div>
       ) : personImg ? (
         <div
-          className="w-full bg-center bg-repeat font-mainFont bg-gradient-to-b from-[#000000] to-[#2322BB] desktop1:pt-[45px]"
+          className="w-full bg-center bg-repeat bg-cover font-mainFont bg-gradient-to-b from-[#000000] to-[#2322BB] desktop1:pt-[45px]"
           id="home"
           style={{ backgroundImage: `url(${content.texts.hero.heroImg})` }}
         >
