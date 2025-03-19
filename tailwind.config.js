f0a401cbedbec59483c9f15df9675dbf05059bca
Gabriel de Souza da Silva <biell@MacBook-Air-de-Gabriel.local>
2025-03-19 13:13:52 -0300

    fix: Insere a paleta de cor do site

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
   extend: {
     colors: {
-      primary: "#CBBDA9",
-      secondary: "#000000",
+      primary: "#F4992D",
+      secondary: "#132448",
       bgSectionLight: "#FFFFFF",
       bgSectionDark: "#182337",
       links: "#0189BB",
