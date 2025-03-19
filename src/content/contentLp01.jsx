928e786f499b5b1efcd39ad16ee602873d2798ce
Gabriel de Souza da Silva <biell@MacBook-Air-de-Gabriel.local>
2025-03-19 10:04:31 -0300

    fix: Preenche a Feature da Lpv

--- src/content/contentLp01.jsx
+++ src/content/contentLp01.jsx
@@ -39,104 +39,34 @@ const contentLp01 = {
   },
   features: {
     sectionHeader: {
-      miniTag: "ISENÇÃO DE IMPOSTO",
+      miniTag: "QUEM TEM DIREITO?",
       title:
-        "Você tem direito à isenção ou restituição do Imposto de Renda? Descubra agora!",
+        "Descubra quem pode solicitar pensão alimentícia e receber o suporte adequado.",
     },
     cards: {
       card1: {
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-hand-coins"
-          >
-            <path d="M11 15h2a2 2 0 1 0 0-4h-3c-.6 0-1.1.2-1.4.6L3 17" />
-            <path d="m7 21 1.6-1.4c.3-.4.8-.6 1.4-.6h4c1.1 0 2.1-.4 2.8-1.2l4.6-4.4a2 2 0 0 0-2.75-2.91l-4.2 3.9" />
-            <path d="m2 16 6 6" />
-            <circle cx="16" cy="9" r="2.9" />
-            <circle cx="6" cy="5" r="3" />
-          </svg>
-        ),
-        title: "Isenção do Imposto de Renda",
+        icon: "A_Definir",
+        title: "Filhos menores de idade",
         description:
-          "Aposentados e pensionistas com doenças graves podem ter direito à isenção do IR. ",
+          "Quando os filhos são menores, ambos os pais têm a responsabilidade de prover seu sustento. A pensão alimentícia garante que as necessidades básicas sejam atendidas.",
       },
       card2: {
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-circle-dollar-sign"
-          >
-            <circle cx="12" cy="12" r="10" />
-            <path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8" />
-            <path d="M12 18V6" />
-          </svg>
-        ),
-        title: "Restituição de valores pagos",
+        icon: "A_Definir",
+        title: "Filhos maiores de idade em dependência",
         description:
-          "Se você pagou imposto indevidamente, é possível recuperar esses valores.",
+          "Mesmo após a maioridade, filhos que ainda dependem financeiramente dos pais por motivo de estudos ou saúde podem solicitar pensão alimentícia.",
       },
       card3: {
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-coins"
-          >
-            <circle cx="8" cy="8" r="6" />
-            <path d="M18.09 10.37A6 6 0 1 1 10.34 18" />
-            <path d="M7 6h1v4" />
-            <path d="m16.71 13.88.7.71-2.82 2.82" />
-          </svg>
-        ),
-        title: "Doenças que garantem o benefício",
+        icon: "A_Definir",
+        title: "Ex-cônjuge em situação de necessidade",
         description:
-          "Diversas doenças dão direito à isenção do IR, como câncer, cardiopatia grave e mais.",
+          "Em casos de separação, o cônjuge que não tem condições de se manter pode solicitar pensão alimentícia, desde que comprove a necessidade.",
       },
       card4: {
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-house"
-          >
-            <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
-            <path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-          </svg>
-        ),
-        title: "Suporte jurídico especializado",
+        icon: "A_Definir",
+        title: "Pais com deficiência ou impossibilidade de trabalho",
         description:
-          "Conte com uma advogada especializada para garantir sua isenção ou restituição sem complicações.",
+          "Pais que não conseguem se sustentar devido a uma deficiência ou incapacidade temporária também podem ter direito a pensão alimentícia para garantir o seu bem-estar.",
       },
     },
   },
@@ -147,131 +77,54 @@ const contentLp01 = {
       alt: "Logomarca",
     },
     sectionHeader: {
-      miniTag: "EXPERTISE EM ISENÇÃO DE IMPOSTO DE RENDA",
-      title: "Isenção e restituição com compromisso e eficiência",
+      miniTag: "A_Definir",
+      title: "A_Definir",
     },
     paragraph: (
       <p>
-        Pagar imposto indevidamente é um problema que muitos aposentados e
-        pensionistas enfrentam. Por isso, atuo de forma ágil e eficiente para
-        garantir seus direitos. Se você tem uma doença grave, pode ter direito à
-        isenção ou até à restituição do Imposto de Renda já pago.
-        <br />
-        <br />
-        Minha missão é encontrar a melhor solução jurídica para você. Entre em
-        contato e descubra como podemos ajudar!
+        A_Definir <br />
+        <br /> A_Definir
       </p>
     ),
-    buttonLabel: "Solicite sua isenção agora",
+    buttonLabel: "Quero falar com um especialista",
     ctaButtonAriaLabel: "Botão para chamada de ação para contato pelo whatsapp",
   },
   whyUs: {
     sectionHeader: {
-      miniTag: "A ESCOLHA CERTA",
-      title: "Por que nos escolher para garantir sua isenção ou restituição?",
+      miniTag: "A_Definir",
+      title: "A_Definir",
     },
     cards: {
       card1: {
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-hourglass"
-          >
-            <path d="M5 22h14" />
-            <path d="M5 2h14" />
-            <path d="M17 22v-4.172a2 2 0 0 0-.586-1.414L12 12l-4.414 4.414A2 2 0 0 0 7 17.828V22" />
-            <path d="M7 2v4.172a2 2 0 0 0 .586 1.414L12 12l4.414-4.414A2 2 0 0 0 17 6.172V2" />
-          </svg>
-        ),
-        title: "Análise detalhada para sua isenção",
-        description:
-          "Cada caso é único. Avaliamos sua situação com atenção para garantir sua isenção ou restituição de forma segura e sem complicações.",
+        icon: "A_Definir",
+        title: "A_Definir",
+        description: "A_Definir",
       },
       card2: {
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-history"
-          >
-            <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
-            <path d="M3 3v5h5" />
-            <path d="M12 7v5l4 2" />
-          </svg>
-        ),
-        title: "Processo rápido e sem burocracia",
-        description:
-          "Atuamos com agilidade para que você tenha acesso ao seu direito sem enrolação ou processos demorados.",
+        icon: "A_Definir",
+        title: "A_Definir",
+        description: "A_Definir",
       },
       card3: {
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-shield-check"
-          >
-            <path d="M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z" />
-            <path d="m9 12 2 2 4-4" />
-          </svg>
-        ),
-        title: "Compromisso total com você",
-        description:
-          "Acompanhamos seu caso de perto, oferecendo suporte personalizado em todas as etapas do processo.",
+        icon: "A_Definir",
+        title: "A_Definir",
+        description: "A_Definir",
       },
       card4: {
-        icon: (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="lucide lucide-thumbs-up"
-          >
-            <path d="M7 10v12" />
-            <path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2a3.13 3.13 0 0 1 3 3.88Z" />
-          </svg>
-        ),
-        title: "Experiência que faz a diferença",
-        description:
-          "Com anos de atuação, sabemos exatamente como garantir sua isenção ou restituição com segurança e eficiência.",
+        icon: "A_Definir",
+        title: "A_Definir",
+        description: "A_Definir",
       },
     },
   },
   cta: {
     bgImg: bgImg,
     sectionHeader: {
-      miniTag: "SEU DIREITO SEM COMPLICAÇÃO",
-      title: "Garanta sua isenção ou restituição agora mesmo!",
-      subtitle: `Se você é aposentado ou pensionista com doença grave, pode ter direito à isenção ou até à restituição do Imposto de Renda. Nossa equipe especializada está pronta para te ajudar a garantir esse benefício de forma rápida e sem burocracia. Não deixe dinheiro para o governo à toa!`,
+      miniTag: "A_Definir",
+      title: "A_Definir",
+      subtitle: `A_Definir`,
     },
-    buttonLabel: "Falar com uma especialista agora",
+    buttonLabel: "Quero falar com um especialista",
     ctaButtonAriaLabel: "Botão para chamada de ação para contato pelo whatsapp",
   },
   contact: {
