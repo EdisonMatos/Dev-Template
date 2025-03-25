556d2c99df6c3972921f84097912f497c84d177f
Gabriel de Souza da Silva <biell@MacBook-Air-de-Gabriel.local>
2025-03-25 16:57:17 -0300

    fix: CTA

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -386,9 +386,11 @@ const content = {
     },
     cta: {
       backgroundImg: ctaWhatsappImgBg,
-      miniTag: "A_Definir",
-      title: "A_Definir",
-      subtitle: "A_Definir",
+      miniTag: "FALE CONOSCO",
+      title:
+        "Enfrentando um problema jurídico? Deixe que cuidamos disso para você!",
+      subtitle:
+        "Converse com nossa equipe. Estamos prontos para oferecer a orientação jurídica de que você precisa.",
       ctaButtonText: "Entre em contato pelo WhatsApp",
     },
     steps: {
