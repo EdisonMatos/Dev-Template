1b6f440cd8f2c6a79a064f0cb91f2fa6991ac561
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-03-27 10:12:09 -0300

    fix: Imagem Hero

--- src/components/sectionElements/hero/HeroPersonImg.jsx
+++ src/components/sectionElements/hero/HeroPersonImg.jsx
@@ -12,14 +12,14 @@ export default function HeroPersonImg({ appDownloadButtons }) {
       className="w-full bg-center desktop1:hidden bg-repeat bg-cover font-mainFont bg-black desktop1:pt-[45px]"
       id="home"
       style={{
-        // backgroundImage: `url(${content.texts.hero.images.backgroundPerson})`,
+        backgroundImage: `url(${content.texts.hero.images.backgroundPerson})`,
        
       }}
     >
       <div className="relative z-10 flex w-full bg-black bg-opacity-0 items-left">
         <div className="w-full text-secondary justify-evenly">
           <div className="h-[100px] desktop1:h-[140px]" />
-          <div className="flex flex-col-reverse gap-[40px] desktop1:flex-row desktop1:justify-between mx-auto w-[90%] max-w-[1215px] items-center pb-[64px] pt-[40px] desktop1:pt-[0px] desktop1:pb-[0px]">
+          <div className="flex flex-col-reverse gap-[10px] desktop1:flex-row desktop1:justify-between mx-auto w-[90%] max-w-[1215px] items-center pb-[64px] pt-[40px] desktop1:pt-[0px] desktop1:pb-[0px]">
             <div className="flex  flex-col w-full desktop1:w-[50%] desktop1:mr-[20px]  ">
               <MotionDivDownToUp>
                 <div className="flex justify-center w-auto text-center desktop1:justify-start desktop1:text-left font-secondFont text-paragraph4">
