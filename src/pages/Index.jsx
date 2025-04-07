95025dc456fb1398e25e56088ce095dc832b560a
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-07 11:33:09 -0300

    fix: ImagemHero

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -20,9 +20,9 @@ export default function Index() {
       <Navbar LightMode={false} />
       <Hero
         appDownloadButtons={false}
-        defaultHero={false}
-        influencer={true}
-        mesclado={false}
+        defaultHero={true}
+        influencer={false}
+        mesclado={true}
       />
 
       <Features
