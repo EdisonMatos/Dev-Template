de65b67eaa2510755feb267c20d0c47e34cc0b26
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-08 12:02:24 -0300

    fix: Altera Rota

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <Routes>
       <Route path="/" element={<Index />} />
-      <Route path="/trabalhista" element={<Lp01 />} />
+      <Route path="/isencao" element={<Lp01 />} />
       {/* <Route path="/whatsapp" element={<WhatsAppLinks />} /> */}
 
       {/* <Route path="/lp02" element={<Lp02 />} /> */}
