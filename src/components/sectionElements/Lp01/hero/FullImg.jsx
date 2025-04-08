dc50b9be11a5561a2533cdf0b01f58f34b310fc7
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-08 11:38:51 -0300

    fix: Favico e PreviewIcon

--- src/components/sectionElements/Lp01/hero/FullImg.jsx
+++ src/components/sectionElements/Lp01/hero/FullImg.jsx
@@ -99,7 +99,7 @@ function FullImg() {
         >
           <SectionWrapper className="">
             <div className="flex w-full">
-              <div className="flex phone1:flex-col-reverse gap-[40px] desktop1:flex-row desktop1:justify-between mx-auto w-[90%] max-w-[1215px] items-center pb-[64px] pt-[46px] desktop1:pt-[68px] desktop1:pb-[96px]">
+              <div className="flex phone1:flex-col-reverse gap-[40px] desktop1:flex-row desktop1:justify-between  w-full max-w-[1215px] items-center pb-[64px] pt-[46px] desktop1:pt-[68px] desktop1:pb-[96px]">
                 <div className="flex flex-col w-full desktop1:w-[50%] desktop1:mr-[20px]">
                   <MotionDivDownToUp className="flex justify-start tablet1:justify-center desktop1:justify-start">
                     <img
