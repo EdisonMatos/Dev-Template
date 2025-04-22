9331e731e071adabf5ff9cb5c2b7aa995d8bb2f4
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-22 11:15:59 -0300

    fix: Reseta content

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -58,9 +58,9 @@ export const infos = {
   emailSecundario: "A_Definir", // Email desejado pelo cliente
   domain: "A_Definir", // Sem "www"
   phone: {
-    ddd: "13",
-    firstPart: "99143", // Apenas os 5 primeiros números (incluindo o 9)
-    secondPart: "0427", // Apenas os 4 últimos números
+    ddd: "00",
+    firstPart: "00000", // Apenas os 5 primeiros números (incluindo o 9)
+    secondPart: "0000", // Apenas os 4 últimos números
   },
   phoneSecundario: {
     ddd: "A_Definir",
