276e8b7a937f457b8e1035d795de3949f7dfd771
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-22 11:53:00 -0300

    fix: Texto Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -124,20 +124,21 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag: "ADVOGADO DE REGISTRO DE MARCAS EM SÃO PAULO",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Garanta sua marca.
+          <span className="text-primary"> Não corra riscos.</span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle:
+        "Marcas não registradas viram alvo fácil. Proteja o que é seu antes que alguém leve.",
       heroImgPhone: heroImgPhone, // img da pessoa mobile
       heroDesktop: heroImgDesktop, //bg com foto da pessoa
       heroImg: bgHeroImg, //fundo da Hero
       heroDefaultImage: heroDefaultImg, //imagem da heroDefault Mobile
       backgroundPerson: backgroundPersonMobile,
-      ctaButtonText: "A_Definir",
+      ctaButtonText: "Fale com especialista",
       obsHero: {
         icon: (
           <svg
@@ -157,7 +158,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "A_Definir",
+        text: "Atendimento presencial em SP e online para todo Brasil",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
