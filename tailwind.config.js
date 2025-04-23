18b86ef3c46f3f7ac61d7838cbcb308a0d38004c
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-23 08:48:50 -0300

    fix: Insere Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
 export const theme = {
   extend: {
     colors: {
-      primary: "#E6007E",
+      primary: "#E3C696",
       secondary: "#000000",
       bgSectionLight: "#FFFFFF",
-      bgSectionDark: "#008F39",
+      bgSectionDark: "#460106",
       links: "#0189BB",
       buttonColor: "#DBB14C",
       lighter: "#FFFFFF",
