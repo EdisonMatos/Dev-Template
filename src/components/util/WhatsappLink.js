83ff54f9c0ef3698609d0a3884f1f9af9860b1b8
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-30 08:07:24 -0300

    fix: Revisa botoões do site e  suas rotas

--- src/components/util/WhatsappLink.js
+++ src/components/util/WhatsappLink.js
@@ -1,4 +1,4 @@
-export const USE_REAL_WHATSAPP_LINK = false;
+export const USE_REAL_WHATSAPP_LINK = true;
 
 export const WHATSAPP_DIRECT_LINK = "https://wa.me/5521972613067";
 export const WHATSAPP_LOCAL_ROUTE = "/whatsapp";
