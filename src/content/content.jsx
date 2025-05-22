a75a8c7436b3285cf0f7a04835a4607d783919e2
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-22 10:45:34 -0300

    fix: Texto Features

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -184,15 +184,16 @@ const content = {
       },
     },
     features: {
-      miniTag: "A_Definir",
-      title: "A_Definir",
-      subtitle: "A_Definir",
+      miniTag: "ÁREA DE ATUAÇÃO",
+      title: "Especialista em BPC/LOAS para idosos e pessoas com deficiênci",
+      subtitle:
+        "Atuação dedicada à conquista e manutenção do benefício assistencial para quem mais precisa.",
       imgFeatures: imgFeatures,
       imgDivisor: imgDivisor,
       card1: {
         img: featuresImg1,
-        title: "A_Definir",
-        subtitle: "A_Definir",
+        title: "Solicitação do BPC para pessoa com deficiência",
+        subtitle: "Garantimos o acesso ao BPC para pessoas com deficiência",
         description: (
           <div>
             A_Definir
@@ -219,8 +220,8 @@ const content = {
       },
       card2: {
         img: featuresImg2,
-        title: "A_Definir",
-        subtitle: "A_Definir",
+        title: "Recursos e revisões de benefício negado",
+        subtitle: "Suporte para recorrer de negativas do INSS ao BPC",
         description: (
           <div>
             A_Definir
@@ -247,8 +248,8 @@ const content = {
       },
       card3: {
         img: featuresImg3,
-        title: "A_Definir",
-        subtitle: "A_Definir",
+        title: "Solicitação do BPC para idosos",
+        subtitle: "Obtenção do BPC para idosos 65+ com baixa renda",
         description: (
           <div>
             A_Definir <br />
@@ -274,8 +275,8 @@ const content = {
       },
       card4: {
         img: featuresImg4,
-        title: "A_Definir",
-        subtitle: "A_Definir",
+        title: "Atualização cadastral e manutenção do BPC",
+        subtitle: "Atualização do CadÚnico e manutenção do BPC ativo",
         description: (
           <div>
             A_Definir
