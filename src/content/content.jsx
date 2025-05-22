6729105da7eaadbde844141f945cad94b5905ac7
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-22 10:58:24 -0300

    fix: Texto Steps

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -452,31 +452,35 @@ const content = {
       ctaButtonText: "Entre em contato pelo WhatsApp",
     },
     steps: {
-      miniTag: "A_Definir",
-      title: "A_Definir",
+      miniTag: "PASSO A PASSO",
+      title: "Como funciona nosso atendimento?",
       subtitle: "",
       img: imgSteps,
       alt: "",
       cards: {
         card1: {
           stepNumber: 1,
-          cardTitle: "A_Definir",
-          cardDescription: "A_Definir",
+          cardTitle: "Consulta inicial",
+          cardDescription:
+            "Realizamos uma reunião para entender seu caso e identificar suas necessidades relacionadas ao BPC/LOAS.",
         },
         card2: {
           stepNumber: 2,
-          cardTitle: "A_Definir",
-          cardDescription: "A_Definir",
+          cardTitle: "Análise detalhada",
+          cardDescription:
+            "Examinamos documentos, laudos médicos e informações para ter uma visão completa do seu direito ao benefício.",
         },
         card3: {
           stepNumber: 3,
-          cardTitle: "A_Definir",
-          cardDescription: "A_Definir",
+          cardTitle: "Elaboração da estratégia",
+          cardDescription:
+            "Desenvolvemos uma estratégia jurídica personalizada para garantir seu acesso ao benefício com eficiência.",
         },
         card4: {
           stepNumber: 4,
-          cardTitle: "A_Definir",
-          cardDescription: "A_Definir",
+          cardTitle: "Ação legal",
+          cardDescription:
+            "Entramos com recursos e ações necessárias para assegurar seu direito ao BPC/LOAS junto ao INSS ou na justiça.",
         },
       },
     },
