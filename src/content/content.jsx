84e331002c9c96e94f78d4e673a2350946c769ed
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-26 12:12:13 -0300

    fix: Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -123,20 +123,21 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag: "ADVOGADA CÍVEL EM ILHABELA - SP",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Ela evitou 5 anos de
+          <span className="text-primary"> dor de cabeça</span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle:
+        "Regularizou o imóvel da família sem processo, sem briga e sem gastar o que não tinha.",
       heroImgPhone: heroImgPhone, // img da pessoa mobile
       heroDesktop: heroImgDesktop, //bg com foto da pessoa
       heroImg: bgHeroImg, //fundo da Hero
       heroDefaultImage: heroDefaultImg, //imagem da heroDefault Mobile
       backgroundPerson: backgroundPersonMobile,
-      ctaButtonText: "A_Definir",
+      ctaButtonText: "Quero uma solução assim",
       obsHero: {
         icon: (
           <svg
@@ -156,7 +157,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "A_Definir",
+        text: "Atendimento presencial e online para todo Brasil",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
