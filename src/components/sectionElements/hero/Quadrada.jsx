c7ea7b5af511555f27aef1d20fadf221b33f70d1
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-28 11:21:53 -0300

    fix: Ajuste Hero Quadrada

--- src/components/sectionElements/hero/Quadrada.jsx
+++ src/components/sectionElements/hero/Quadrada.jsx
@@ -73,7 +73,7 @@ export default function Quadrada({ appDownloadButtons, colorMode }) {
                       {content.texts.hero.obsHero.icon}
 
                       <p
-                        className={`ml-[10px] text-center mt-[12px] desktop1:mt-0 ${obsTextColor}`}
+                        className={`ml-[10px] text-center desktop1:text-start mt-[12px] desktop1:mt-0 ${obsTextColor}`}
                       >
                         {content.texts.hero.obsHero.text}
                       </p>
