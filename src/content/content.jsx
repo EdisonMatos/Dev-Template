84337cc551acf6c54f1d2f6935ab579c2b6e89dd
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-28 10:56:40 -0300

    fix: Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -123,20 +123,22 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag:
+        "ESCRITÓRIO E CONSULTORIA JURÍDICA ESPECIALIZADA EM PLANEJAMENTO PATRIMONIAL E SUCESSÓRIO",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Quem planeja,
+          <span className="text-primary"> paga menos impostos</span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle:
+        "Evite conflitos e reduza custos com um planejamento patrimonial seguro e estratégico.",
       heroImgPhone: heroImgPhone, // img da pessoa mobile
       heroDesktop: heroImgDesktop, //bg com foto da pessoa
       heroImg: bgHeroImg, //fundo da Hero
       heroDefaultImage: heroDefaultImg, //imagem da heroDefault Mobile
       backgroundPerson: backgroundPersonMobile,
-      ctaButtonText: "A_Definir",
+      ctaButtonText: "Quero organizar meu patrimônio",
       obsHero: {
         icon: (
           <svg
@@ -156,7 +158,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "A_Definir",
+        text: "Atendimento presencial em Itapema - SC e online para todo Brasil",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
