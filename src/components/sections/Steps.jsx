04b85ef25e5d5cf32b510fa67079f560048d63b8
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-30 11:27:36 -0300

    fix: Steps

--- src/components/sections/Steps.jsx
+++ src/components/sections/Steps.jsx
@@ -45,7 +45,7 @@ export default function Steps({ colorMode }) {
             sectionHeaderTitle={content.texts.steps.title}
             sectionHeaderSubtitle={content.texts.steps.subtitle}
             color={colorMode ? "light" : ""}
-            type={colorMode ? "article" : ""}
+            type={colorMode ? "" : "article"}
             colorMode={colorMode}
           />
 
