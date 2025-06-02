9af230f63250fdc4825d2ddb16c8664fcf1e68f7
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-02 11:16:42 -0300

    fix: Hero

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -124,20 +124,21 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag: "ADVOGADO MILITAR EM SÃO LUÍS - MA",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Nem todo advogado
+          <span className="text-primary"> entende farda</span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle:
+        "Atuação estratégica e experiente na defesa de quem serve o país com honra e disciplina",
       heroImgPhone: heroImgPhone, // img da pessoa mobile
       heroDesktop: heroImgDesktop, //bg com foto da pessoa
       heroImg: bgHeroImg, //fundo da Hero
       heroDefaultImage: heroDefaultImg, //imagem da heroDefault Mobile
       backgroundPerson: backgroundPersonMobile,
-      ctaButtonText: "A_Definir",
+      ctaButtonText: "Fale com quem entende",
       obsHero: {
         icon: (
           <svg
@@ -157,7 +158,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "A_Definir",
+        text: "Atendimento 100% digital com a facilidade de um clique e a confiança de um escritório físico.",
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
@@ -406,7 +407,7 @@ const content = {
         miniTag: "A_Definir",
         title: "A_Definir",
         subtitle: "A_Definir",
-        paragraph: <p>A_Definir</p>,
+        paragraph: <p></p>,
         labelInstagram: "Siga-nos no Instagram",
         labelFacebook: "Siga-nos no Facebook",
         labelLinkedin: "Siga-nos no LinkedIn",
