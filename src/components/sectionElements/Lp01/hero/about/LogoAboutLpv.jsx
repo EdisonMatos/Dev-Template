684090fc04febb931ef659ccf21b3645f35cdddb
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-09 12:48:06 -0300

    fix: Ajustes no About

--- src/components/sectionElements/Lp01/hero/about/LogoAboutLpv.jsx
+++ src/components/sectionElements/Lp01/hero/about/LogoAboutLpv.jsx
@@ -4,11 +4,11 @@ import MotionDivDownToUp from "../../../../animation/MotionDivDownToUp";
 
 function LogoAboutLpv() {
   return (
-    <MotionDivDownToUp className=" w-[100%]  desktop1:w-[415px] desktop2:w-[485px] flex justify-start">
+    <MotionDivDownToUp className=" w-[100%] justify-center  desktop1:w-[415px] desktop2:w-[485px] flex desktop1:justify-start">
       <img
         src={contentLp01.about.imgAbout.img}
         alt={contentLp01.about.logo.alt}
-        className="w-[70%] tablet1:w-[55.5%] desktop1:w-[70%] bg-bgSectionLight rounded-xl p-[10px]"
+        className="w-[90%] tablet1:w-[55.5%] desktop1:w-[90%] bg-bgSectionLight rounded-xl p-[10px]"
       />
     </MotionDivDownToUp>
   );
