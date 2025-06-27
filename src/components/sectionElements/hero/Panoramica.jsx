2cedf8e562b3976ed0c4a4b5de1459634d3b98de
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-27 15:08:38 -0300

    fix: Hero

--- src/components/sectionElements/hero/Panoramica.jsx
+++ src/components/sectionElements/hero/Panoramica.jsx
@@ -47,13 +47,13 @@ export default function Panoramica({ appDownloadButtons }) {
                   </div>
                 </MotionDivDownToUp>
                 <MotionDivDownToUp>
-                  <div className="text-colorWhite flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title6">
+                  <div className="text-white flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title6">
                     <h1 className="">{content.texts.hero.title}</h1>
                   </div>
                 </MotionDivDownToUp>
                 <MotionDivDownToUp>
                   <div className="flex justify-center w-full text-center desktop1:text-left desktop1:justify-start font-secondFont text-paragraph4 phone3:text-paragraph5">
-                    <p className="text-colorWhite mb-[32px] ">
+                    <p className="text-white mb-[32px] ">
                       {content.texts.hero.subtitle}
                     </p>
                   </div>
@@ -72,7 +72,7 @@ export default function Panoramica({ appDownloadButtons }) {
                     <div className="flex justify-center desktop1:justify-start">
                       <div className="flex flex-col items-center desktop1:flex-row text-primary">
                         {content.texts.hero.obsHero.icon}
-                        <p className="text-colorWhite  ml-[10px] text-center mt-[12px] desktop1:mt-0">
+                        <p className="text-white  ml-[10px] text-center mt-[12px] desktop1:mt-0">
                           {content.texts.hero.obsHero.text}
                         </p>
                       </div>
@@ -81,7 +81,7 @@ export default function Panoramica({ appDownloadButtons }) {
                   {appDownloadButtons && (
                     <MotionDivDownToUp>
                       <div className="flex flex-col items-center desktop1:items-start">
-                        <p className="mb-[20px] mt-[60px] text-colorWhite  text-center">
+                        <p className="mb-[20px] mt-[60px] text-white  text-center">
                           {content.texts.hero.app.description}
                         </p>
                         <div className="">
@@ -160,13 +160,13 @@ export default function Panoramica({ appDownloadButtons }) {
                       </div>
                     </MotionDivDownToUp>
                     <MotionDivDownToUp>
-                      <div className="text-colorWhite flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7">
+                      <div className="text-white flex justify-center desktop1:justify-start font-bold leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7">
                         <h1 className="">{content.texts.hero.title}</h1>
                       </div>
                     </MotionDivDownToUp>
                     <MotionDivDownToUp>
                       <div className="flex justify-center w-full text-center desktop1:text-left desktop1:justify-start font-secondFont text-paragraph4 phone3:text-paragraph5">
-                        <p className="text-colorWhite mb-[32px] ">
+                        <p className="text-white mb-[32px] ">
                           {content.texts.hero.subtitle}
                         </p>
                       </div>
@@ -185,7 +185,7 @@ export default function Panoramica({ appDownloadButtons }) {
                         <div className="flex justify-center desktop1:justify-start">
                           <div className="flex flex-col items-center desktop1:flex-row text-primary">
                             {content.texts.hero.obsHero.icon}
-                            <p className="text-colorWhite  ml-[10px] text-center mt-[12px] desktop1:mt-0">
+                            <p className="text-white  ml-[10px] text-center mt-[12px] desktop1:mt-0">
                               {content.texts.hero.obsHero.text}
                             </p>
                           </div>
@@ -194,7 +194,7 @@ export default function Panoramica({ appDownloadButtons }) {
                       {appDownloadButtons && (
                         <MotionDivDownToUp>
                           <div className="flex flex-col items-center desktop1:items-start">
-                            <p className="mb-[20px] mt-[60px] text-colorWhite  text-center">
+                            <p className="mb-[20px] mt-[60px] text-white  text-center">
                               {content.texts.hero.app.description}
                             </p>
                             <div className="">
--- src/content/content.jsx
+++ src/content/content.jsx
@@ -124,20 +124,21 @@ const content = {
       ctaButtonTextResponsive: "Contato",
     },
     hero: {
-      miniTag: "A_Definir",
+      miniTag: "ADVOGADO EM SAÚDE EM FLORIANÓPOLIS - SC",
       title: (
         <h1 className="mb-[16px]">
-          A_Definir
-          <span className="text-primary"> A_Definir</span>
+          Negaram seu direito à saúde?
+          <span className="text-primary"> </span>
         </h1>
       ),
-      subtitle: "A_Definir",
+      subtitle:
+        "Quando o plano falha, eu entro em ação. Defesa rápida e técnica para quem não pode esperar.",
       heroImgPhone: heroImgPhone, // img da pessoa mobile
       heroDesktop: heroImgDesktop, //bg com foto da pessoa
       heroImg: bgHeroImg, //fundo da Hero
       heroDefaultImage: heroDefaultImg, //imagem da heroDefault Mobile
       backgroundPerson: backgroundPersonMobile,
-      ctaButtonText: "A_Definir",
+      ctaButtonText: "Exija seus direitos agora",
       obsHero: {
         icon: (
           <svg
@@ -157,7 +158,11 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: "A_Definir",
+        text: (
+          <p>
+            Atendimento presencial e online via <i>WhatsApp</i> para todo Brasil
+          </p>
+        ),
       },
       ctaButtonAriaLabel:
         "Botão para chamada de ação para contato pelo whatsapp",
