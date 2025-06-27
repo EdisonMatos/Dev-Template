f87350e9f498088013ec648415b6091a753b5b76
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-27 15:05:05 -0300

    fix: infos e rede social

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -40,18 +40,18 @@ export default function Index() {
         paragraphsModal={false}
       />
       <About modal={true} showGallery={false} colorMode={colorMode} />
-      {/* <AboutInstagram
+      <AboutInstagram
         colorMode={colorMode}
         instagram={true}
         socialPrint={true}
-      /> */}
+      />
       <Cta colorMode={colorMode} />
       <Steps colorMode={colorMode} />
       {/* <Maps colorMode={colorMode} /> */}
       <Faq colorMode={colorMode} />
       <FooterSocial
         colorMode={colorMode}
-        addres={false}
+        addres={true}
         addresSecundario={false}
         phoneSecundario={false}
         phoneTerciario={false}
