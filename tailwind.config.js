a873bc84d63be9bc207c0997a33ae1aec93d7840
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-06-27 14:55:04 -0300

    fix: infos, sitemap, robots e metatags

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ export const theme = {
       buttonColor: "#DBB14C",
       iconButtons: "#000",
       labelButtons: "#000",
-      minititle: "#57A595",
+      minititle: "#D3D3D3",
 
       // Sombras
 
