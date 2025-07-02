cefc8d15a9e318563cc7078e0260dd854066f1bb
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-02 14:59:09 -0300

    fix: favico, previewIcon e imagens

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -405,7 +405,7 @@ const content = {
         miniTag: "A_Definir",
         title: "A_Definir",
         subtitle: "A_Definir",
-        paragraph: <p>A_Definir</p>,
+        paragraph: <p></p>,
         labelInstagram: "Siga-nos no Instagram",
         labelFacebook: "Siga-nos no Facebook",
         labelLinkedin: "Siga-nos no LinkedIn",
