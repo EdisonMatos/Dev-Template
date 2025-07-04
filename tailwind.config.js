66df7437c8fdd88aa6b710c62c04bd1d4a94c75d
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-04 16:07:19 -0300

    fix: infos e Metatags

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,11 +25,13 @@ export const theme = {
       // Botões e Links
 
       links: "#0189BB",
-      buttonColor: "#DBB14C",
-      iconButtons: "#000",
-      labelButtons: "#000",
-      minititle: "#57A595",
-
+      buttonColor: "#000000",
+      iconButtons: "#fff",
+      labelButtons: "#fff",
+      minititleLightLabel: "#fff", //label das minitag de seções claras
+      minititleDarkLabel: "#fff", //label minitag de seções escuras
+      minititleDark: "#000", //bg minitag de seções claras
+      minititle: "#000", //bg minitag de seções escuras
       // Sombras
 
       shadowHero: "#000000",
