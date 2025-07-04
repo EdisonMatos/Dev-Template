685512e719978bf2dd0b6eacda8f747f58e37ec7
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-07-04 16:34:51 -0300

    fix: Cta

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export const theme = {
       // Fundos de Seção
 
       bgSectionLight: "#FFFFFF",
-      bgSectionDark: "#990000",
+      bgSectionDark: "#000000",
       bgSectionOpacityLight: "#F7F7F7",
       bgFixedLight: "#E8E8E8",
       bgSectionOpacityDark: "#111111",
